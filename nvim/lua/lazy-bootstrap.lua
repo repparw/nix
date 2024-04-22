2fd6edf3a5b1547dcfd28ca57298e8153466cb1e
repparw <ubritos@gmail.com>
2024-04-22 00:00:02 -0300

    Auto-Commit

---
+++ nvim/lua/lazy-bootstrap.lua
@@ -0,0 +1,14 @@
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
