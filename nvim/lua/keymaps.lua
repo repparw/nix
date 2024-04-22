0a9f98d87c753458d2b4c5a7028eef0e7a16ef9c
repparw <ubritos@gmail.com>
2024-04-22 00:00:02 -0300

    Auto-Commit

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,27 @@
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- Visual line wraps
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.keymap.set({'n', 'v'}, '<leader>s', ':update<CR>') -- Save
+vim.cmd [[inoremap <expr><tab> pumvisible() ? "\<c-n>" : "\<tab>"]]
+vim.cmd [[inoremap <expr><s-tab> pumvisible() ? "\<c-p>" : "<s-tab>"]]
