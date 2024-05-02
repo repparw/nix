941b1c0a641f6bc7c86ccb1d6d423cc2e04fbf4c
repparw <ubritos@gmail.com>
2024-05-02 00:00:00 -0300

    Auto-Commit

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -23,5 +23,3 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 vim.keymap.set({'n', 'v'}, '<leader>s', ':update<CR>') -- Save
-vim.cmd [[inoremap <expr><tab> pumvisible() ? "\<c-n>" : "\<tab>"]]
-vim.cmd [[inoremap <expr><s-tab> pumvisible() ? "\<c-p>" : "<s-tab>"]]
