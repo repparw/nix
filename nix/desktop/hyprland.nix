1145aaf839d3aedbde5de13f32ce0d10fe079778
repparw <ubritos@gmail.com>
2024-05-18 00:00:00 -0300

    Auto-Commit

---
+++ nix/desktop/hyprland.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+	# Desktop
+	swaylock
+	swayidle
+	swaynotificationcenter
+	waybar
+	tofi
+	hyprpicker
+	wl-clipboard
+	# hyprwm/contrib
+	inputs.hyprland-contrib.packages.${pkgs.system}.grimblast
+	inputs.hyprland-contrib.packages.${pkgs.system}.hdrop
+  	];
+
+  ]
+}
