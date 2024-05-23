71aab30baa331245854d28d154efaca7972c8510
repparw <ubritos@gmail.com>
2024-05-22 21:45:45 -0300

    Test push vm

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,26 +1,39 @@
 {
   description = "repparw's flake";
 
-  outputs = { self, ... }@inputs:
+  outputs = { self, home-manager, ... }@inputs:
 	let
 
 		system = "x86_64-linux";
+		systemSettings = {
+				hostName = "beta";
+		};
 
 		pkgs = inputs.nixpkgs.legacyPackages.${system};
-		unstable = inputs.nixpkgs-unstable.legacyPackages.${system};
+		stable = inputs.nixpkgs-stable.legacyPackages.${system};
 
 	in {
 	  nixosConfigurations = {
 		beta = inputs.nixpkgs.lib.nixosSystem {
 		  modules = [
-			./hosts/beta/configuration.nix
-			inputs.stylix.nixosModules.stylix { image = "./gruvbox.jpg"; }
-			inputs.home-manager.nixosModules.home-manager
+			./hosts/${systemSettings.hostName}/configuration.nix
+#			inputs.stylix.nixosModules.stylix
+			home-manager.nixosModules.home-manager {
+			home-manager.useGlobalPkgs = true;
+			home-manager.useUserPackages = true;
+			home-manager.users.repparw = import ./hosts/${systemSettings.hostName}/home.nix;
+			home-manager.extraSpecialArgs = {
+			  inherit system;
+			  inherit pkgs;
+			  inherit stable; 
+			  inherit inputs;
+			  };
+			}
 			];
 		  specialArgs = {
 			inherit system;
 			inherit pkgs;
-			inherit unstable; 
+			inherit stable; 
 			inherit inputs;
 		  };
 		};
@@ -29,19 +42,19 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-23.11"; # Modification: Correct URL syntax
-    };
-	nixpkgs-unstable = {
       url = "github:NixOS/nixpkgs/nixpkgs-unstable"; # Modification: Correct URL syntax
     };
+	nixpkgs-stable = {
+      url = "github:NixOS/nixpkgs/nixos-23.11"; # Modification: Correct URL syntax
+    };
     hyprland-nix = {
-      url = "github:hyprland-community/hyprnix";
+      url = "github:hyprland-community/hyprnix"; # Follows unstable
     };
     stylix = {
       url = "github:danth/stylix";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager"; # Follows unstable, has branches for stable
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
