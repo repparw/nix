491f132b4042981e4fddda89e1fd82e7aada0638
repparw <ubritos@gmail.com>
2024-05-31 18:26:48 -0300

    nix to 24.05 t440

--- nix/hosts/beta/configuration.nix
+++ nix/hosts/beta/configuration.nix
@@ -145,6 +145,8 @@
   hardware.xone.enable = true;
   hardware.xpadneo.enable = true;
 
+  services.logind.lidSwitchExternalPower = "ignore";
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
