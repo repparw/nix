ed9f658c534d42cad6a0faa6be81c92d3c50991f
repparw <ubritos@gmail.com>
2024-06-05 00:00:00 -0300

    Auto-Commit

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,24 +1,11 @@
 return {
-    {
         "folke/trouble.nvim",
-        config = function()
-            require("trouble").setup({
-                icons = false,
-            })
-
-            vim.keymap.set("n", "<leader>tt", function()
-                require("trouble").toggle()
-            end)
-
-            vim.keymap.set("n", "[t", function()
-                require("trouble").next({skip_groups = true, jump = true});
-            end)
-
-            vim.keymap.set("n", "]t", function()
-                require("trouble").previous({skip_groups = true, jump = true});
-            end)
-
-        end
-    },
+		opts = {},
+		cmd = "Trouble",
+		keys = {
+				{ "<leader>tt", "<cmd>Trouble quickfix toggle<cr>", desc = "[T]rouble [T]oggle" },
+				{ "[t", "<cmd>Trouble previous", desc = "[T]rouble previous" },
+				{ "]t", "<cmd>Trouble next", desc = "[T]rouble next" },
+		},
 }
 
