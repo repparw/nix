b86f41f029b88fcf0405983fce449d8ea6b6b2ce
repparw <ubritos@gmail.com>
2024-07-19 16:49:18 -0300

    tlp for t440

--- nix/hosts/beta/default.nix
+++ nix/hosts/beta/default.nix
@@ -29,6 +29,8 @@
   # ignore lid close on AC power
   services.logind.lidSwitchExternalPower = "ignore";
 
+  services.tlp.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
