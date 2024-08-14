82a719f232dfec35f8f2d1cdff8c301ec154f87d
repparw <ubritos@gmail.com>
2024-08-14 17:44:01 -0300

    Auto-commit

--- nix/hosts/alpha/default.nix
+++ nix/hosts/alpha/default.nix
@@ -171,6 +171,7 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+  networking.firewall.trustedInterfaces = [ "enp42s0" ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
