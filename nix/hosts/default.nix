799c957adf3623a0097d5958ac3805d1d05b875e
repparw <ubritos@gmail.com>
2024-08-18 21:00:00 -0300

    Autocommit

--- nix/hosts/default.nix
+++ nix/hosts/default.nix
@@ -1,10 +1,21 @@
 { hostName, ... }:
 
 {
-  imports = [ ./${hostName} ];
+  imports = [
+    ../../modules/nixos/cachix.nix
+    ../../modules/nixos/common.nix
+    ../../modules/nixos/hyprland.nix
+    ./${hostName}
+  ];
 
   nixpkgs.config.allowUnfree = true;
 
   nix.optimise.automatic = true;
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
 
 }
