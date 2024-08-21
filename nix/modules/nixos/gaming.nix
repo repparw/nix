8ff0a31ccff996bee3764d9591693a46b66a3237
repparw <ubritos@gmail.com>
2024-08-21 16:28:04 -0300

    Autocommit

---
+++ nix/modules/nixos/gaming.nix
@@ -0,0 +1,18 @@
+{ ... }:
+{
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    gamescopeSession.enable = true;
+  };
+
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
+
+  programs.gamemode = {
+    enable = true;
+    enableRenice = true;
+  };
+}
