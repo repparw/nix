09da4c0bbe16e589982a1f320c9f51f7e44c302d
repparw <ubritos@gmail.com>
2024-08-29 18:52:03 -0300

    Autocommit

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -1,45 +1,43 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*",  -- recommended, use latest release instead of latest commit
-  lazy = true,
-  event = {
-     "BufReadPre /home/repparw/Documents/obsidian/**.md",
-     "BufNewFile /home/repparw/Documents/obsidian/**.md",
-  },
-  dependencies = {
-    -- Required.
-    "nvim-lua/plenary.nvim",
-
-  },
-  opts = {
-    workspaces = {
-      {
-        name = "obsidian",
-        path = "~/Documents/obsidian",
-      },
-    },
-	mappings = {
-	  ["gf"] = {
-      action = function()
-        return require("obsidian").util.gf_passthrough()
-      end,
-      opts = { noremap = false, expr = true, buffer = true },
-    },
-	  ["<leader>o"] = {
-	  action = function()
-		  return require("obsidian").util.ObsidianOpen()
-	   -- return require("obsidian").get_client():command("ObsidianNew", { args = "Foo" })
-	  end,
-	  opts = { noremap = false, expr = true, buffer = true },
+	"epwalsh/obsidian.nvim",
+	version = "*", -- recommended, use latest release instead of latest commit
+	lazy = true,
+	event = {
+		"BufReadPre /home/repparw/Documents/obsidian/**.md",
+		"BufNewFile /home/repparw/Documents/obsidian/**.md",
 	},
-    -- Smart action depending on context, either follow link or toggle checkbox.
-	  ["<cr>"] = {
-	  action = function()
-		return require("obsidian").util.smart_action()
-	  end,
-	  opts = { buffer = true, expr = true },
-    },
-  },
- },
-
+	dependencies = {
+		-- Required.
+		"nvim-lua/plenary.nvim",
+	},
+	opts = {
+		workspaces = {
+			{
+				name = "obsidian",
+				path = "~/Documents/obsidian",
+			},
+		},
+		mappings = {
+			["gf"] = {
+				action = function()
+					return require("obsidian").util.gf_passthrough()
+				end,
+				opts = { noremap = false, expr = true, buffer = true },
+			},
+			["<leader>o"] = {
+				action = function()
+					return require("obsidian").get_client():command("ObsidianOpen")
+					-- return require("obsidian").get_client():command("ObsidianNew", { args = "Foo" })
+				end,
+				opts = { noremap = false, expr = true, buffer = true },
+			},
+			-- Smart action depending on context, either follow link or toggle checkbox.
+			["<cr>"] = {
+				action = function()
+					return require("obsidian").util.smart_action()
+				end,
+				opts = { buffer = true, expr = true },
+			},
+		},
+	},
 }
