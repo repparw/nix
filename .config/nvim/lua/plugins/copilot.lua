431fea084565284947915764e3df23278c64fac6
repparw <ubritos@gmail.com>
2024-09-07 16:25:29 -0300

    rm copilotchat nvim

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -16,13 +16,4 @@ return {
 			require("copilot_cmp").setup()
 		end,
 	},
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		branch = "canary",
-		dependencies = {
-			{ "zbirenbaum/copilot.lua" },
-			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-		},
-		opts = {},
-	},
 }
