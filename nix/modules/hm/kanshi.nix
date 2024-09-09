d6db5189e5fa036571b309648fb3dd7b5d2ed3ef
repparw <ubritos@gmail.com>
2024-09-09 13:39:29 -0300

    kanshi fixed

--- nix/modules/hm/kanshi.nix
+++ nix/modules/hm/kanshi.nix
@@ -2,24 +2,32 @@
 
 {
   services.kanshi = {
-	enable = true;
-	systemdTarget = "hyprland-session.target";
-	settings = [
-	  { profile.name = "undocked";
-		profile.outputs = [{
-			criteria = "eDP-1";
-		  }];
-	  }
-	  { profile.name = "docked";
-		profile.outputs = [{
-		  criteria = "eDP-1";
-		  status = "disable";
-		}
-		{
-		 criteria = "ViewSonic Corporation XG2401 SERIES UG2174400580";
-		}
-	  ];
-	  }
-	];
+    enable = true;
+    systemdTarget = "graphical-session.target";
+    settings = [
+      {
+        profile.name = "docked";
+        profile.outputs = [
+          {
+            criteria = "eDP-1";
+            status = "disable";
+          }
+          {
+            criteria = "DP-4";
+            status = "enable";
+            mode = "1920x1080@60";
+          }
+        ];
+      }
+      {
+        profile.name = "undocked";
+        profile.outputs = [
+          {
+            criteria = "eDP-1";
+            status = "enable";
+          }
+        ];
+      }
+    ];
   };
 }
