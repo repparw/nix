3da36111170414930fd6f7b8df4b15220e539eae
repparw <ubritos@gmail.com>
2024-09-21 21:24:00 -0300

    Autocommit

--- nix/hosts/beta/home.nix
+++ nix/hosts/beta/home.nix
@@ -1,22 +1,13 @@
-{ pkgs, unstable, ... }:
+{ pkgs, ... }:
 {
   imports = [
-    ../../modules/hm/cli.nix
-    ../../modules/hm/gui.nix
-    ../../modules/hm/hypr/hyprland.nix
     ../../modules/hm/kanshi.nix # Dynamic display
   ];
 
   # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  home.username = "repparw";
-  home.homeDirectory = "/home/repparw";
 
   home.packages = with pkgs; [
     brightnessctl # backlight
   ];
-
-  home.stateVersion = "23.11";
 
 }
