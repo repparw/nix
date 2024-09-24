ae1d93bdbc37c5a0e2630379ced5008abf34a93e
repparw <ubritos@gmail.com>
2024-09-24 15:36:02 -0300

    Autocommit

--- nix/hosts/alpha/default.nix
+++ nix/hosts/alpha/default.nix
@@ -25,11 +25,14 @@
     shell = pkgs.zsh;
     description = "repparw";
     extraGroups = [
+      "adbusers"
       "networkmanager"
       "wheel"
       "docker"
     ];
   };
+
+  programs.adb.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
