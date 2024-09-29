dd7bbf5d374455bf0d224033c1791ac1717eadf1
repparw <ubritos@gmail.com>
2024-09-28 22:40:07 -0300

    Autocommit

--- nix/modules/hm/gui.nix
+++ nix/modules/hm/gui.nix
@@ -120,8 +120,70 @@
     enable = true;
     package = unstable.spotify-player;
     keymaps = { };
-    settings = { };
-    themes = { };
+    settings = {
+      theme = "gruvbox_dark";
+      client_id = "2728200c381a418983c3de5b30bc77a9";
+      client_port = 8080;
+      playback_format = "" "
+{track} • {artists}
+{album}
+{metadata}" "";
+      tracks_playback_limit = 50;
+      app_refresh_duration_in_ms = 32;
+      playback_refresh_duration_in_ms = 0;
+      page_size_in_rows = 20;
+      play_icon = "▶";
+      pause_icon = "▌▌";
+      liked_icon = "♥";
+      border_type = "Plain";
+      progress_bar_type = "Rectangle";
+      playback_window_position = "Top";
+      cover_img_length = 9;
+      cover_img_width = 5;
+      cover_img_scale = 1.0;
+      playback_window_width = 6;
+      enable_streaming = "Always";
+      enable_cover_image_cache = true;
+      default_device = "spotifyd";
+      enable_notify = false;
+      copy_command = {
+        command = "wl-copy";
+        args = [ ];
+      };
+      device = {
+        name = "Terminal UI";
+        device_type = "computer";
+        volume = 40;
+        bitrate = 320;
+        audio_cache = false;
+        normalization = false;
+      };
+    };
+    themes = [
+      {
+        name = "gruvbox_dark";
+        palette = {
+          background = "#282828";
+          foreground = "#ebdbb2";
+          black = "#282828";
+          red = "#cc241d";
+          green = "#98971a";
+          yellow = "#d79921";
+          blue = "#458588";
+          magenta = "#b16286";
+          cyan = "#689d6a";
+          white = "#a89984";
+          bright_black = "#928374";
+          bright_red = "#fb4934";
+          bright_green = "#b8bb26";
+          bright_yellow = "#fabd2f";
+          bright_blue = "#83a598";
+          bright_magenta = "#d3869b";
+          bright_cyan = "#8ec07c";
+          bright_white = "#ebdbb2";
+        };
+      }
+    ];
   };
 
   programs.feh = {
