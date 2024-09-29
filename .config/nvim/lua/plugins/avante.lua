0a4b68b1cf1b1333961e602de6bfc40ce1dca19d
repparw <ubritos@gmail.com>
2024-09-29 16:48:04 -0300

    Autocommit

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -12,6 +12,45 @@ return {
 			toggle = "<leader>At",
 			refresh = "<leader>Ar",
 		},
+		keys = function(_, keys)
+			---@type avante.Config
+			local opts = require("lazy.core.plugin").values(
+				require("lazy.core.config").spec.plugins["avante.nvim"],
+				"opts",
+				false
+			)
+
+			local mappings = {
+				{
+					opts.mappings.ask,
+					function()
+						require("avante.api").ask()
+					end,
+					desc = "avante: ask",
+					mode = { "n", "v" },
+				},
+				{
+					opts.mappings.refresh,
+					function()
+						require("avante.api").refresh()
+					end,
+					desc = "avante: refresh",
+					mode = "v",
+				},
+				{
+					opts.mappings.edit,
+					function()
+						require("avante.api").edit()
+					end,
+					desc = "avante: edit",
+					mode = { "n", "v" },
+				},
+			}
+			mappings = vim.tbl_filter(function(m)
+				return m[1] and #m[1] > 0
+			end, mappings)
+			return vim.list_extend(mappings, keys)
+		end,
 	},
 	-- if you want to download pre-built binary, then pass source=false. Make sure to follow instruction above.
 	-- Also note that downloading prebuilt binary is a lot faster comparing to compiling from source.
