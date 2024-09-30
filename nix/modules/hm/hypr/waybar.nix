56ebe3b0b954c12267ab1f91015d03d507fce0a0
repparw <ubritos@gmail.com>
2024-09-30 15:48:04 -0300

    Autocommit

--- nix/modules/hm/hypr/waybar.nix
+++ nix/modules/hm/hypr/waybar.nix
@@ -47,166 +47,166 @@
             "default" = "";
           };
         };
-			"mpris": {
-	"dynamic-len": 70,
-	"artist-len": 20,
-	"dynamic-importance-order": ["title", "position", "length", "album", "artist" ],
-    "format": "{player_icon} {dynamic}",
-    "format-paused": "{status_icon} <i>{dynamic}</i>",
-    "player-icons": {
-      "default": "󰲸",
-      "firefox": "󰈹",
-      "mpv": "󰐌"
-    },
-    "status-icons": {
-      "paused": "󰏤",
-      "playing": "",
-      "stopped": ""
-    }
-	},
-    "hyprland/submap": {
-        "format": "<span style=\"italic\">{}</span>"
-    },
-    "hyprland/window": {
-        "format": "{}",
-        "max-length": 50, 
-        "tooltip": false
-    },
-    "bluetooth": {
-        "interval": 30,
-        "format": "{icon}",
-        // "format-alt": "{status}",
-        "format-icons": {
-            "enabled": "",
-            "disabled": ""
-        },
-        "on-click": "blueman-manager"
-    },
-    "hyprland/language": {
-        "format": " {}",
-        "max-length": 5,
-        "min-length": 4
-    },
-    "tray": {
-        //"icon-size": 14,
-        "spacing": 6
-    },
-    "clock/time": {
-        "timezone": "America/Argentina/Buenos_Aires",
-        "interval": 10,
-        "format": "  {:%H:%M}",
-        "tooltip-format": "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>"
-    },
-    "network": {
-        // "interface": "wlan0", // (Optional) To force the use of this interface,
-        "format-wifi": "<span color=\"#ebdbb2\"></span>  {essid}",
-        "format-ethernet": "󰛳",
-        "format-linked": "{ifname} (No IP) ",
-        "format-disconnected": "󰅛",
-        "family": "ipv4",
-        "tooltip-format-wifi": "  {ifname} @ {essid}\nIP: {ipaddr}\nStrength: {signalStrength}%\nFreq: {frequency}MHz\n {bandwidthUpBits}  {bandwidthDownBits}",
-        "tooltip-format-ethernet": "󰈁 {ifname}\nIP: {ipaddr}\n {bandwidthUpBits}  {bandwidthDownBits}",
-        "on-click": "kitty nmtui"
-    },
-    "pulseaudio": {
-        "scroll-step": 3, // %, can be a float
-        "format": "{icon} {volume:2}% {format_source}",
-        "format-bluetooth": "{icon} {volume}%  {format_source}",
-        "format-bluetooth-muted": "{icon}   {format_source}",
-        "format-muted": " {format_source}",
-        "format-source": "",
-        "format-source-muted": "<span color=\"#fb4833\"></span>",
-        "format-icons": {
-            "headphone": "",
-            "hands-free": "",
-            "headset": "",
-            "phone": "",
-            "portable": "",
-            "car": "",
-            "default": ["", "", ""]
-        },
-        "on-click": "pavucontrol",
-        "on-click-right": "pactl set-source-mute @DEFAULT_SOURCE@ toggle"
-    },
-	"battery": {
-        	"states": {
-            		"warning": 30,
-            		"critical": 15
-        	},
-        	"format": "{icon} {capacity}%",
-        	"format-charging": "󰂄 {capacity}%",
-        	"format-plugged": "󱐋{capacity}%",
-        	"format-alt": "{time} {icon}",
-        	"format-full": "󰂄 {capacity}%",
-        	"format-icons": ["󰁻", "󰁽", "󰂁"]
-	},
-    "custom/arrow1": {
-        "format": "",
-        "tooltip": false
-    },
-    "custom/arrow2": {
-        "format": "",
-        "tooltip": false
-    },
+	"mpris"= {
+	dynamic-len= 70;
+	artist-len= 20;
+	dynamic-importance-order= ["title", "position", "length", "album", "artist" ];
+    format= "{player_icon} {dynamic}";
+    format-paused= "{status_icon} <i>{dynamic}</i>";
+    player-icons= {
+      default= "󰲸";
+      firefox= "󰈹";
+      mpv= "󰐌";
+    };
+    status-icons= {
+      paused= "󰏤";
+      playing= "";
+      stopped= "";
+    };
+	};
+    "hyprland/submap"= {
+        format= "<span style=\"italic\">{}</span>"
+    };
+    "hyprland/window"= {
+        format= "{}";
+        max-length= 50, 
+        tooltip= false
+    };
+    "bluetooth"= {
+        interval= 30;
+        format= "{icon}";
+        // format-alt= "{status}";
+        format-icons= {
+            enabled= "";
+            disabled= ""
+        };
+        on-click= "blueman-manager"
+    };
+    "hyprland/language"= {
+        format= " {}";
+        max-length= 5;
+        min-length= 4
+    };
+    "tray"= {
+        //icon-size= 14;
+        spacing= 6
+    };
+    "clock/time"= {
+        timezone= "America/Argentina/Buenos_Aires";
+        interval= 10;
+        format= "  {:%H:%M}";
+        tooltip-format= "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>"
+    };
+    "network"= {
+        // interface= "wlan0", // (Optional) To force the use of this interface;
+        format-wifi= "<span color=\"#ebdbb2\"></span>  {essid}";
+        format-ethernet= "󰛳";
+        format-linked= "{ifname} (No IP) ";
+        format-disconnected= "󰅛";
+        family= "ipv4";
+        tooltip-format-wifi= "  {ifname} @ {essid}\nIP: {ipaddr}\nStrength: {signalStrength}%\nFreq: {frequency}MHz\n {bandwidthUpBits}  {bandwidthDownBits}";
+        tooltip-format-ethernet= "󰈁 {ifname}\nIP: {ipaddr}\n {bandwidthUpBits}  {bandwidthDownBits}";
+        on-click= "kitty nmtui"
+    };
+    "pulseaudio"= {
+        scroll-step= 3, // %, can be a float
+        format= "{icon} {volume:2}% {format_source}";
+        format-bluetooth= "{icon} {volume}%  {format_source}";
+        format-bluetooth-muted= "{icon}   {format_source}";
+        format-muted= " {format_source}";
+        format-source= "";
+        format-source-muted= "<span color=\"#fb4833\"></span>";
+        format-icons= {
+            headphone= "";
+            hands-free= "";
+            headset= "";
+            phone= "";
+            portable= "";
+            car= "";
+            default= ["", "", ""]
+        };
+        on-click= "pavucontrol";
+        on-click-right= "pactl set-source-mute @DEFAULT_SOURCE@ toggle"
+    };
+	"battery"= {
+        	states= {
+            		warning= 30;
+            		critical= 15
+        	};
+        	format= "{icon} {capacity}%";
+        	format-charging= "󰂄 {capacity}%";
+        	format-plugged= "󱐋{capacity}%";
+        	format-alt= "{time} {icon}";
+        	format-full= "󰂄 {capacity}%";
+        	format-icons= ["󰁻", "󰁽", "󰂁"]
+	};
+    "custom/arrow1"= {
+        format= "";
+        tooltip= false;
+    };
+    "custom/arrow2"= {
+        format= "";
+        tooltip= false;
+    };
 
-    "custom/arrow3": {
-        "format": "",
-        "tooltip": false
-    },
+    "custom/arrow3"= {
+        format= "";
+        tooltip= false;
+    };
 
-    "custom/arrow4": {
-        "format": "",
-        "tooltip": false
-    },
+    "custom/arrow4"= {
+        format= "";
+        tooltip= false;
+    };
 
-    "custom/arrow5": {
-        "format": "",
-        "tooltip": false
-    },
+    "custom/arrow5"= {
+        format= "";
+        tooltip= false;
+    };
 
-    "custom/arrow6": {
-        "format": "",
-        "tooltip": false
-    },
+    "custom/arrow6"= {
+        format= "";
+        tooltip= false;
+    };
 
-    "custom/arrow7": {
-        "format": "",
-        "tooltip": false
-    },
+    "custom/arrow7"= {
+        format= "";
+        tooltip= false;
+    };
 
-    "custom/arrow8": {
-        "format": "",
-        "tooltip": false
-    },
+    "custom/arrow8"= {
+        format= "";
+        tooltip= false;
+    };
 
-    "custom/arrow9": {
-        "format": "",
-        "tooltip": false
-    },
+    "custom/arrow9"= {
+        format= "";
+        tooltip= false;
+    };
 
-    "custom/arrow10": {
-        "format": "",
-        "tooltip": false
-    },
-	"custom/notification": {
-    "tooltip": false,
-    "format": "{icon}",
-    "format-icons": {
-      "notification": "<span foreground='red'><sup></sup></span>",
-      "none": "",
-      "dnd-notification": "<span foreground='red'><sup></sup></span>",
-      "dnd-none": "",
-      "inhibited-notification": "<span foreground='red'><sup></sup></span>",
-      "inhibited-none": "",
-      "dnd-inhibited-notification": "<span foreground='red'><sup></sup></span>",
-      "dnd-inhibited-none": ""
-    },
-    "return-type": "json",
-    "exec-if": "which swaync-client",
-    "exec": "swaync-client -swb",
-    "on-click": "swaync-client -t -sw",
-    "on-click-right": "swaync-client -d -sw",
-    "escape": true
+    "custom/arrow10"= {
+        format= "";
+        tooltip= false;
+    };
+	"custom/notification"= {
+    tooltip= false;
+    format= "{icon}";
+    format-icons= {
+      notification= "<span foreground='red'><sup></sup></span>";
+      none= "";
+      dnd-notification= "<span foreground='red'><sup></sup></span>";
+      dnd-none= "";
+      inhibited-notification= "<span foreground='red'><sup></sup></span>";
+      inhibited-none= "";
+      dnd-inhibited-notification= "<span foreground='red'><sup></sup></span>";
+      dnd-inhibited-none= ""
+    };
+    return-type= "json";
+    exec-if= "which swaync-client";
+    exec= "swaync-client -swb";
+    on-click= "swaync-client -t -sw";
+    on-click-right= "swaync-client -d -sw";
+    escape= true;
 	}
 
 
