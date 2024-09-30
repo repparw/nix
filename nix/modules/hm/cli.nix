cdd00a9e0b1602b46401ba10aca2f6303f128bfb
repparw <ubritos@gmail.com>
2024-09-29 21:28:17 -0300

    Autocommit

--- nix/modules/hm/cli.nix
+++ nix/modules/hm/cli.nix
@@ -14,66 +14,62 @@
   };
 
   programs.tmux = {
-      enable = true;
+    enable = true;
     shell = "${pkgs.zsh}/bin/zsh";
     terminal = "xterm-kitty";
     historyLimit = 10000;
-	prefix = "C-a";
-	mouse = true;
-	baseIndex = 1;
-	newSession = true;
-	keyMode = "vi";
-    plugins = with pkgs.tmuxPlugins;
-      [
-	  pain-control
-	  #power-zoom
-	  #tmux-floax TODO
-	  vim-tmux-navigator
-	  yank
-	  resurrect
-	  continuum
+    prefix = "C-a";
+    mouse = true;
+    baseIndex = 1;
+    newSession = true;
+    keyMode = "vi";
+    plugins = with pkgs.tmuxPlugins; [
+      pain-control
+      #power-zoom
+      #tmux-floax TODO
+      vim-tmux-navigator
+      yank
+      resurrect
+      continuum
 
-	  gruvbox
-      ];
+      gruvbox
+    ];
     extraConfig = ''
-		bind-key @ command-prompt -p "create pane from:" "join-pane -s ':%%'"
+      bind-key @ command-prompt -p "create pane from:" "join-pane -s ':%%'"
 
-		# Shift Alt vim keys to switch windows
-		bind -n M-H previous-window
-		bind -n M-L next-window
+      # Shift Alt vim keys to switch windows
+      bind -n M-H previous-window
+      bind -n M-L next-window
 
-		set-option -g update-environment "DISPLAY WAYLAND_DISPLAY SSH_AUTH_SOCK"
+      set-option -g update-environment "DISPLAY WAYLAND_DISPLAY SSH_AUTH_SOCK"
 
-		set -g @resurrect-dir "$XDG_DATA_HOME/tmux/resurrect"
-		set -g @continuum-restore 'on'
+      set -g @resurrect-dir "$XDG_DATA_HOME/tmux/resurrect"
+      set -g @continuum-restore 'on'
 
-		# THEME
-		# disable unused functionality in gruvbox theme
-		set -g @gruvbox-show-battery false
-		set -g @gruvbox-show-network false
-		set -g @gruvbox-show-timezone false
-		set -g @gruvbox-show-weather false
-		set -g @gruvbox-show-fahrenheit false
-		# enable non default functionality in gruvbox theme
-		set -g @gruvbox-show-left-icon session
-		set -g @gruvbox-show-powerline true
-		set -g @gruvbox-military-time true
-		set -g @gruvbox-day-month true
+      # THEME
+      # disable unused functionality in gruvbox theme
+      set -g @gruvbox-show-battery false
+      set -g @gruvbox-show-network false
+      set -g @gruvbox-show-timezone false
+      set -g @gruvbox-show-weather false
+      set -g @gruvbox-show-fahrenheit false
+      # enable non default functionality in gruvbox theme
+      set -g @gruvbox-show-left-icon session
+      set -g @gruvbox-show-powerline true
+      set -g @gruvbox-military-time true
+      set -g @gruvbox-day-month true
 
-		bind C-l send-keys 'C-l'
+      bind C-l send-keys 'C-l'
 
-		# keybindings
-		bind-key -T copy-mode-vi v send-keys -X begin-selection
-		bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
-		bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+      # keybindings
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
 
-		bind '"' split-window -v -c "#{pane_current_path}"
-		bind % split-window -h -c "#{pane_current_path}"
-
+      bind '"' split-window -v -c "#{pane_current_path}"
+      bind % split-window -h -c "#{pane_current_path}"
     '';
   };
-}
-
 
   programs.ssh.addKeysToAgent = "yes";
 
