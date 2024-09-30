f606ce16278e03c4d36b24200a298e59a3cc8bec
repparw <ubritos@gmail.com>
2024-09-29 21:20:16 -0300

    Autocommit

--- nix/modules/hm/cli.nix
+++ nix/modules/hm/cli.nix
@@ -18,6 +18,9 @@
     shell = "${pkgs.zsh}/bin/zsh";
     terminal = "xterm-kitty";
     historyLimit = 10000;
+	prefix = "C-a";
+	mouse = true;
+	baseIndex = 1;
     plugins = with pkgs.tmuxPlugins;
       [
 	  sensible
@@ -28,9 +31,31 @@
 	  yank
 	  resurrect
 	  continuum
+
+	  gruvbox
       ];
     extraConfig = ''
+		bind-key @ command-prompt -p "create pane from:" "join-pane -s ':%%'"
 
+		set-option -g update-environment "DISPLAY WAYLAND_DISPLAY SSH_AUTH_SOCK"
+
+		set -g @resurrect-dir "$XDG_DATA_HOME/tmux/resurrect"
+		set -g @continuum-restore 'on'
+
+		# THEME
+		# disable unused functionality in gruvbox theme
+		set -g @gruvbox-show-battery false
+		set -g @gruvbox-show-network false
+		set -g @gruvbox-show-timezone false
+		set -g @gruvbox-show-weather false
+		set -g @gruvbox-show-fahrenheit false
+		# enable non default functionality in gruvbox theme
+		set -g @gruvbox-show-left-icon session
+		set -g @gruvbox-show-powerline true
+		set -g @gruvbox-military-time true
+		set -g @gruvbox-day-month true
+
+		bind C-l send-keys 'C-l'
     '';
   };
 }
