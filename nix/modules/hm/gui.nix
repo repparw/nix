05f647db83e17e7df115306ff75ff31c2dae6cf5
repparw <ubritos@gmail.com>
2024-09-30 14:44:18 -0300

    Autocommit

--- nix/modules/hm/gui.nix
+++ nix/modules/hm/gui.nix
@@ -35,12 +35,12 @@
 
   programs.kitty = {
     enable = true;
+    themeFile = "GruvboxMaterialDarkMedium";
     font = {
       name = "FiraCode Nerd Font Mono";
       size = 12;
     };
     settings = {
-      theme = "Gruvbox Material Dark Medium";
       disable_ligatures = "cursor";
       enable_audio_bell = "no";
       window_margin = "2 2 0";
