4fb0442b7ff8d593787103d348f4f0383e79aea3
repparw <ubritos@gmail.com>
2024-09-29 21:36:11 -0300

    Autocommit

--- nix/modules/hm/cli.nix
+++ nix/modules/hm/cli.nix
@@ -24,14 +24,41 @@
     newSession = true;
     keyMode = "vi";
     plugins = with pkgs.tmuxPlugins; [
-      gruvbox
+      {
+        plugin = gruvbox;
+        extraConfig = ''
+                # THEME
+                # disable unused functionality in gruvbox theme
+                set -g @gruvbox-show-battery false
+                set -g @gruvbox-show-network false
+                set -g @gruvbox-show-timezone false
+                set -g @gruvbox-show-weather false
+                set -g @gruvbox-show-fahrenheit false
+                # enable non default functionality in gruvbox theme
+                set -g @gruvbox-show-left-icon session
+                set -g @gruvbox-show-powerline true
+                set -g @gruvbox-military-time true
+                set -g @gruvbox-day-month true
+          	  '';
+      }
+      {
+        plugin = resurrect;
+        extraConfig = ''
+          set -g @resurrect-dir "$XDG_DATA_HOME/tmux/resurrect"
+        '';
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+        '';
+      }
 
       pain-control
       #power-zoom
       #tmux-floax TODO
       vim-tmux-navigator
       yank
-      resurrect
       continuum
     ];
     extraConfig = ''
@@ -43,21 +70,7 @@
 
       set-option -g update-environment "DISPLAY WAYLAND_DISPLAY SSH_AUTH_SOCK"
 
-      set -g @resurrect-dir "$XDG_DATA_HOME/tmux/resurrect"
-      set -g @continuum-restore 'on'
 
-      # THEME
-      # disable unused functionality in gruvbox theme
-      set -g @gruvbox-show-battery false
-      set -g @gruvbox-show-network false
-      set -g @gruvbox-show-timezone false
-      set -g @gruvbox-show-weather false
-      set -g @gruvbox-show-fahrenheit false
-      # enable non default functionality in gruvbox theme
-      set -g @gruvbox-show-left-icon session
-      set -g @gruvbox-show-powerline true
-      set -g @gruvbox-military-time true
-      set -g @gruvbox-day-month true
 
       bind C-l send-keys 'C-l'
 
