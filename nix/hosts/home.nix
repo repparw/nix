e2adc420bf5fd52d34fdbafbd4b91c691a5865db
repparw <ubritos@gmail.com>
2024-09-30 13:08:00 -0300

    Autocommit

--- nix/hosts/home.nix
+++ nix/hosts/home.nix
@@ -11,11 +11,6 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  nixpkgs.overlays = [
-    (self: super: { mpv = super.mpv.override { scripts = [ self.mpvScripts.mpris ]; }; })
-    inputs.nixvim.overlays.default
-  ];
-
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
