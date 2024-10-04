d7ba5318df50fe1380afde762b809f245e7d4269
repparw <ubritos@gmail.com>
2024-10-04 12:24:00 -0300

    Autocommit

--- nix/modules/nixos/dlsuite.nix
+++ nix/modules/nixos/dlsuite.nix
@@ -5,6 +5,7 @@
   virtualisation.podman = {
     enable = true;
     autoPrune.enable = true;
+    dockerCompat = true;
     defaultNetwork.settings = {
       # Required for container networking to be able to use names.
       dns_enabled = true;
