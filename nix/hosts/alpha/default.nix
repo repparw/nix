92e051fb5fbb6b5d56dc26ed279622a567397d20
repparw <ubritos@gmail.com>
2024-10-04 14:04:02 -0300

    Autocommit

--- nix/hosts/alpha/default.nix
+++ nix/hosts/alpha/default.nix
@@ -195,7 +195,7 @@
   # networking.firewall.enable = false;
   networking.firewall.trustedInterfaces = [
     "enp42s0"
-    "podman1"
+    "docker0"
   ];
 
   # This value determines the NixOS release from which the default
