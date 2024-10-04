477364cc83348c5432920c081d4bbaea121c105c
repparw <ubritos@gmail.com>
2024-10-04 11:52:00 -0300

    Autocommit

---
+++ nix/modules/hm/obs.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+{
+  systemd.user.services."obs" = {
+    Unit = {
+      StartLimitIntervalSec = 60;
+      StartLimitBurst = 4;
+    };
+
+    Service = {
+      ExecStart = [
+        "${pkgs.obs-studio}/bin/obs --disable-shutdown-check --startreplaybuffer --minimize-to-tray"
+      ];
+      Restart = [ "on-failure" ];
+      RestartSec = 1;
+    };
+
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
+}
