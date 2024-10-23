4d340570e87dd3499dca3e7e9936d491b10cf197
repparw <ubritos@gmail.com>
2024-10-23 19:56:00 -0300

    Autocommit

--- nix/modules/hm/gui.nix
+++ nix/modules/hm/gui.nix
@@ -31,23 +31,6 @@
       #add here and uncomment
     ]);
 
-  programs.kitty = {
-    enable = true;
-    themeFile = "GruvboxMaterialDarkMedium";
-    font = {
-      name = "FiraCode Nerd Font Mono";
-      size = 12;
-    };
-    settings = {
-      disable_ligatures = "cursor";
-      enable_audio_bell = "no";
-      window_margin = "2 2 0";
-      window_padding_width = "1 1 0";
-      confirm_os_window_close = 0;
-      background_opacity = "0.9";
-    };
-  };
-
   services.kdeconnect = {
     enable = true;
   };
@@ -89,107 +72,126 @@
     gtk.enable = true;
   };
 
-  programs.chromium.enable = true;
-
-  programs.firefox = {
-    enable = true;
-
-    nativeMessagingHosts = [ pkgs.tridactyl-native ];
-
-    ####profiles = {
-    ####  default = {
-    ####  	isDefault = true;
-    ####	userChrome = (builtins.readFile ../source/userChrome.css);
-    ####	Path="ii5adzcc.default-release";
-    ####	settings = {
-    ####	  "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-    ####	  "layers.acceleration.force-enabled" = true;
-    ####	  "gfx.webrender.all" = true;
-    ####	  "gfx.webrender.enabled" = true;
-    ####	  "layout.css.backdrop-filter.enabled" = true;
-    ####	  "svg.context-properties.content.enabled" = true;
-    ####	};
-    ####  };
-    ####  kiosk = {
-
-    ####  }
-    ####  socials = {
-    ####};
-  };
-
-  programs.feh = {
-    enable = true;
-    buttons = {
-      zoom_in = 4;
-      zoom_out = 5;
+  programs = {
+    kitty = {
+      enable = true;
+      themeFile = "GruvboxMaterialDarkMedium";
+      font = {
+        name = "FiraCode Nerd Font Mono";
+        size = 12;
+      };
+      settings = {
+        disable_ligatures = "cursor";
+        enable_audio_bell = "no";
+        window_margin = "2 2 0";
+        window_padding_width = "1 1 0";
+        confirm_os_window_close = 0;
+        background_opacity = "0.9";
+      };
     };
-    keybindings = {
-      prev_img = "comma";
-      next_img = "period";
+
+    chromium.enable = true;
+
+    firefox = {
+      enable = true;
+
+      nativeMessagingHosts = [ pkgs.tridactyl-native ];
+
+      ####profiles = {
+      ####  default = {
+      ####  	isDefault = true;
+      ####	userChrome = (builtins.readFile ../source/userChrome.css);
+      ####	Path="ii5adzcc.default-release";
+      ####	settings = {
+      ####	  "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+      ####	  "layers.acceleration.force-enabled" = true;
+      ####	  "gfx.webrender.all" = true;
+      ####	  "gfx.webrender.enabled" = true;
+      ####	  "layout.css.backdrop-filter.enabled" = true;
+      ####	  "svg.context-properties.content.enabled" = true;
+      ####	};
+      ####  };
+      ####  kiosk = {
+
+      ####  }
+      ####  socials = {
+      ####};
     };
-  };
 
-  programs.zathura = {
-    enable = true;
-    options = {
-      notification-error-bg = "#32302f"; # bg
-      notification-error-fg = "#fb4934"; # bright:red
-      notification-warning-bg = "#32302f"; # bg
-      notification-warning-fg = "#fabd2f"; # bright:yellow
-      notification-bg = "#32302f"; # bg
-      notification-fg = "#b8bb26"; # bright:green
-
-      completion-bg = "#504945"; # bg2
-      completion-fg = "#ebdbb2"; # fg
-      completion-group-bg = "#3c3836"; # bg1
-      completion-group-fg = "#928374"; # gray
-      completion-highlight-bg = "#83a598"; # bright:blue
-      completion-highlight-fg = "#504945"; # bg2
-
-      # Define the color in index mode
-      index-bg = "#504945"; # bg2
-      index-fg = "#ebdbb2"; # fg
-      index-active-bg = "#83a598"; # bright:blue
-      index-active-fg = "#504945"; # bg2
-
-      inputbar-bg = "#32302f"; # bg
-      inputbar-fg = "#ebdbb2"; # fg
-
-      statusbar-bg = "#504945"; # bg2
-      statusbar-fg = "#ebdbb2"; # fg
-
-      highlight-color = "#fabd2f"; # bright:yellow
-      highlight-active-color = "#fe8019"; # bright:orange
-
-      default-bg = "#32302f"; # bg
-      default-fg = "#ebdbb2"; # fg
-      render-loading = "true";
-      render-loading-bg = "#32302f"; # bg
-      render-loading-fg = "#ebdbb2"; # fg
-
-      # Recolor book content's color
-      recolor-lightcolor = "#32302f"; # bg
-      recolor-darkcolor = "#ebdbb2"; # fg
-      recolor = "true";
-      # set recolor-keephue             true      # keep original color
-
-      guioptions = "";
-
-      sandbox = "none";
-      statusbar-h-padding = 0;
-      statusbar-v-padding = 0;
-      page-padding = 1;
-      selection-clipboard = "clipboard";
+    feh = {
+      enable = true;
+      buttons = {
+        zoom_in = 4;
+        zoom_out = 5;
+      };
+      keybindings = {
+        prev_img = "comma";
+        next_img = "period";
+      };
     };
-    mappings = {
-      u = "scroll half-up";
-      d = "scroll half-down";
-      D = "toggle_page_mode";
-      r = "reload";
-      R = "rotate";
-      K = "zoom in";
-      J = "zoom out";
-      i = "recolor";
+
+    zathura = {
+      enable = true;
+      options = {
+        notification-error-bg = "#32302f"; # bg
+        notification-error-fg = "#fb4934"; # bright:red
+        notification-warning-bg = "#32302f"; # bg
+        notification-warning-fg = "#fabd2f"; # bright:yellow
+        notification-bg = "#32302f"; # bg
+        notification-fg = "#b8bb26"; # bright:green
+
+        completion-bg = "#504945"; # bg2
+        completion-fg = "#ebdbb2"; # fg
+        completion-group-bg = "#3c3836"; # bg1
+        completion-group-fg = "#928374"; # gray
+        completion-highlight-bg = "#83a598"; # bright:blue
+        completion-highlight-fg = "#504945"; # bg2
+
+        # Define the color in index mode
+        index-bg = "#504945"; # bg2
+        index-fg = "#ebdbb2"; # fg
+        index-active-bg = "#83a598"; # bright:blue
+        index-active-fg = "#504945"; # bg2
+
+        inputbar-bg = "#32302f"; # bg
+        inputbar-fg = "#ebdbb2"; # fg
+
+        statusbar-bg = "#504945"; # bg2
+        statusbar-fg = "#ebdbb2"; # fg
+
+        highlight-color = "#fabd2f"; # bright:yellow
+        highlight-active-color = "#fe8019"; # bright:orange
+
+        default-bg = "#32302f"; # bg
+        default-fg = "#ebdbb2"; # fg
+        render-loading = "true";
+        render-loading-bg = "#32302f"; # bg
+        render-loading-fg = "#ebdbb2"; # fg
+
+        # Recolor book content's color
+        recolor-lightcolor = "#32302f"; # bg
+        recolor-darkcolor = "#ebdbb2"; # fg
+        recolor = "true";
+        # set recolor-keephue             true      # keep original color
+
+        guioptions = "";
+
+        sandbox = "none";
+        statusbar-h-padding = 0;
+        statusbar-v-padding = 0;
+        page-padding = 1;
+        selection-clipboard = "clipboard";
+      };
+      mappings = {
+        u = "scroll half-up";
+        d = "scroll half-down";
+        D = "toggle_page_mode";
+        r = "reload";
+        R = "rotate";
+        K = "zoom in";
+        J = "zoom out";
+        i = "recolor";
+      };
     };
   };
 
