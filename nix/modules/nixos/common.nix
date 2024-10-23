39cdb409ed6c68008881023f5138fcf3285f1282
repparw <ubritos@gmail.com>
2024-10-23 18:56:00 -0300

    Autocommit

--- nix/modules/nixos/common.nix
+++ nix/modules/nixos/common.nix
@@ -56,6 +56,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    wl-clipboard # for waydroid sharing
     vim
     zsh
     wget
