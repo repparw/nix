769bcf33abb755cafc0bac4b68cad50d79e24916
repparw <ubritos@gmail.com>
2024-11-16 22:00:01 -0300

    Autocommit

--- nix/hosts/common.nix
+++ nix/hosts/common.nix
@@ -26,6 +26,14 @@
       "repparw"
     ];
 
+    substituters = [
+      "nix-community.cachix.org"
+    ];
+
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+
   };
 
   nix.optimise.automatic = true;
@@ -36,11 +44,4 @@
 
   hardware.keyboard.qmk.enable = true;
 
-  substituters = [
-    "nix-community.cachix.org"
-  ];
-
-  trusted-public-keys = [
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-  ];
 }
