d4f2465446e59d3141802e9f9256fb785a906007
repparw <ubritos@gmail.com>
2024-11-16 21:56:00 -0300

    Autocommit

--- nix/hosts/common.nix
+++ nix/hosts/common.nix
@@ -37,7 +37,10 @@
   hardware.keyboard.qmk.enable = true;
 
   substituters = [
-    "https://cache.nixos.org"
-    "https://nixos.hyprland.org"
+    "nix-community.cachix.org"
+  ];
+
+  trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
   ];
 }
