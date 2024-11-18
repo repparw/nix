0ca88d55025f46546f6953f0e28c3bc930b70aaf
repparw <ubritos@gmail.com>
2024-11-18 17:00:01 -0300

    Autocommit

--- nix/modules/hm/file-manager.nix
+++ nix/modules/hm/file-manager.nix
@@ -5,13 +5,14 @@
       enable = true;
       enableZshIntegration = true;
       settings = {
-        flavor = "gruvbox-dark";
+        flavor = "use gruvbox-dark";
       };
-	  flavors = [
-gruvbox-dark = fetchTarball {
- url = "https://github.com/bennyyip/gruvbox-dark.yazi/archive/refs/heads/main.zip";
- sha256 = "0g3syimbxcayn5l43gw9svaa3g2k8wj3iz6990qbpb7vb0xf5frh";
-  ];
+      flavors = {
+        gruvbox-dark = fetchTarball {
+          url = "https://github.com/bennyyip/gruvbox-dark.yazi/archive/refs/heads/main.zip";
+          sha256 = "0yyw1wsljl1vr2cdd5y9fjd3vwnf2h31y5jnjc4j0dq88gbjh5rl";
+        };
+      };
     };
     zsh.initExtra = ''
       zvm_after_init_commands+=("bindkey -s '^e' 'yazi\n'")
