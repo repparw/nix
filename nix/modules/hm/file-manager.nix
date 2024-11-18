d1c6ef818934245315a02319b9bd746208e0a1b2
repparw <ubritos@gmail.com>
2024-11-18 16:52:00 -0300

    Autocommit

--- nix/modules/hm/file-manager.nix
+++ nix/modules/hm/file-manager.nix
@@ -4,7 +4,9 @@
     yazi = {
       enable = true;
       enableZshIntegration = true;
-
+      settings = {
+        flavor = "gruvbox-dark";
+      };
     };
     zsh.initExtra = ''
       zvm_after_init_commands+=("bindkey -s '^e' 'yazi\n'")
