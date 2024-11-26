f6da2556cceaefc9b9351df3d3a34ef2aacc81bb
repparw <ubritos@gmail.com>
2024-11-26 17:56:06 -0300

    Autocommit

--- nix/modules/hm/hypr/hypr-pkgs.nix
+++ nix/modules/hm/hypr/hypr-pkgs.nix
@@ -28,7 +28,7 @@
       enable = true;
       settings = {
         general = {
-          lock_cmd = "pidof hyprlock || hyprlock";
+          lock_cmd = "hyprlock";
           before_sleep_cmd = "loginctl lock-session";
           after_sleep_cmd = "hyprctl dispatch dpms on";
           ignore_dbus_inhibit = false;
