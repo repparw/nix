943771f29b0a8f78d6329c63f0d8348795cc99ac
repparw <ubritos@gmail.com>
2024-11-26 18:00:00 -0300

    Autocommit

--- nix/modules/hm/hypr/hypr-pkgs.nix
+++ nix/modules/hm/hypr/hypr-pkgs.nix
@@ -30,19 +30,13 @@
         general = {
           lock_cmd = "hyprlock";
           before_sleep_cmd = "loginctl lock-session";
-          after_sleep_cmd = "hyprctl dispatch dpms on";
           ignore_dbus_inhibit = false;
         };
 
         listener = [
           {
             timeout = 900;
-            on-timeout = "hyprctl dispatch dpms off";
-            on-resume = "hyprctl dispatch dpms on";
-          }
-          {
-            timeout = 910;
-            on-timeout = "loginctl lock-session";
+            on-timeout = "systemctl sleep";
           }
         ];
       };
