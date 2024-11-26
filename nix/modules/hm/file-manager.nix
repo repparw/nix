e19419c2dc9b8f8d3a0e0209355f3565ff155114
repparw <ubritos@gmail.com>
2024-11-26 14:36:00 -0300

    Autocommit

--- nix/modules/hm/file-manager.nix
+++ nix/modules/hm/file-manager.nix
@@ -15,9 +15,15 @@
           sha256 = "0yyw1wsljl1vr2cdd5y9fjd3vwnf2h31y5jnjc4j0dq88gbjh5rl";
         };
       };
+      plugins = {
+        smart-enter = fetchTarball {
+          url = "https://github.com/yazi-rs/plugins/tree/main/smart-enter.yazi";
+          sha256 = "0bac6gcdqbl8hvqv3ylykz3nhbc6103p8sll7457mky7pavrwali";
+        };
+      };
+      zsh.initExtra = ''
+        zvm_after_init_commands+=("bindkey -s '^e' 'yazi\n'")
+      '';
     };
-    zsh.initExtra = ''
-      zvm_after_init_commands+=("bindkey -s '^e' 'yazi\n'")
-    '';
   };
 }
