6d31bdae8bcfd2292543563a60625babe230bb50
repparw <ubritos@gmail.com>
2024-11-26 20:36:00 -0300

    Autocommit

--- nix/modules/hm/hypr/hyprland.nix
+++ nix/modules/hm/hypr/hyprland.nix
@@ -5,7 +5,7 @@
   wayland.windowManager.hyprland = {
     enable = true;
     plugins = with pkgs.hyprlandPlugins; [
-      #hyprsplit
+      hyprsplit
     ];
 
     systemd.variables = [ "--all" ];
