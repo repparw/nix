501e89f08554e89d54cbfca9420f9f40f142b640
repparw <ubritos@gmail.com>
2025-02-17 20:44:00 -0300

    Autocommit

---
+++ nix/modules/nixos/virtualisation.nix
@@ -0,0 +1,10 @@
+{ ... }:
+{
+  programs.virt-manager.enable = true;
+
+  users.groups.libvirtd.members = [ "your_username" ];
+
+  virtualisation.libvirtd.enable = true;
+
+  virtualisation.spiceUSBRedirection.enable = true;
+}
