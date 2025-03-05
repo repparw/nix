54160cef40705b691194bc57fc6871abe1e7db75
repparw <ubritos@gmail.com>
2025-03-05 20:00:00 -0300

    Autocommit

--- nix/modules/hm/nixvim.nix
+++ nix/modules/hm/nixvim.nix
@@ -3,5 +3,35 @@
   programs.nixvim = {
     enable = true;
     defaultEditor = true;
+	options = {
+	  splitright = true;
+	  splitbelow = true;
+	  termguicolors = true;
+	  ignorecase = true;
+	  smartcase = true;
+	  tabstop = 4;
+	  shiftwidth = 2;
+	  number = true;
+	  relativenumber = true;
+	  list = true;
+	  listchars = { # eol = '↲';
+		tab = '» ';
+		trail = '·';
+		extends = '<';
+		precedes = '>';
+		conceal = '┊';
+		nbsp = '␣';
+	  };
+	  scrolloff = 10;
+	  inccommand = 'split';
+	  background = 'dark';
+	  showmode = false;
+	  mouse = 'a';
+	  updatetime = 250;
+	  timeoutlen = 300;
+	  cursorline = true;
+	  undofile = true;
+	  conceallevel = 1;
+  };
   };
 }
