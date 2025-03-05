972a7a80da831f09da7b69246e2cb36a10471fc3
repparw <ubritos@gmail.com>
2025-03-05 20:04:05 -0300

    Autocommit

--- nix/modules/hm/nixvim.nix
+++ nix/modules/hm/nixvim.nix
@@ -3,35 +3,36 @@
   programs.nixvim = {
     enable = true;
     defaultEditor = true;
-	options = {
-	  splitright = true;
-	  splitbelow = true;
-	  termguicolors = true;
-	  ignorecase = true;
-	  smartcase = true;
-	  tabstop = 4;
-	  shiftwidth = 2;
-	  number = true;
-	  relativenumber = true;
-	  list = true;
-	  listchars = { # eol = '↲';
-		tab = '» ';
-		trail = '·';
-		extends = '<';
-		precedes = '>';
-		conceal = '┊';
-		nbsp = '␣';
-	  };
-	  scrolloff = 10;
-	  inccommand = 'split';
-	  background = 'dark';
-	  showmode = false;
-	  mouse = 'a';
-	  updatetime = 250;
-	  timeoutlen = 300;
-	  cursorline = true;
-	  undofile = true;
-	  conceallevel = 1;
-  };
+    opts = {
+      splitright = true;
+      splitbelow = true;
+      termguicolors = true;
+      ignorecase = true;
+      smartcase = true;
+      tabstop = 4;
+      shiftwidth = 2;
+      number = true;
+      relativenumber = true;
+      list = true;
+      listchars = {
+        # eol = '↲';
+        tab = "» ";
+        trail = "·";
+        extends = "<";
+        precedes = ">";
+        conceal = "┊";
+        nbsp = "␣";
+      };
+      scrolloff = 10;
+      inccommand = "split";
+      background = "dark";
+      showmode = false;
+      mouse = "a";
+      updatetime = 250;
+      timeoutlen = 300;
+      cursorline = true;
+      undofile = true;
+      conceallevel = 1;
+    };
   };
 }
