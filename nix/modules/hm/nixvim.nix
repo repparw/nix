d07525b0eb83d473420dd69cb9774f11e45f38a7
repparw <ubritos@gmail.com>
2025-03-05 20:20:07 -0300

    Autocommit

--- nix/modules/hm/nixvim.nix
+++ nix/modules/hm/nixvim.nix
@@ -51,45 +51,46 @@
       telescope = {
         enable = true;
         keymaps = {
-          "<C-p>" = {
-            action = "git_files";
-			options = { desc = "Find project files"};
-          };
-				"<leader>fh" = {
-										action = "help_tags";
-										options = { desc = "[F]ind [H]elp"};
-        };
-		"<leader>fk" = {
-									action = "keymaps";
-									options = { desc = "[F]ind [K]eymaps"};
-
-        };
-		"<leader>ff" = {
-								action = "find_files";
-								options = { desc = "[F]ind [F]iles"};
-								};
-								"<leader>fs" = {
-									action = "builtin";
-									options = { desc = "[F]ind [S]elect Telescope"};
-								};
-								"<leader>fw" = {
-									action = "grep_string";
-									options = { desc = "[F]ind current [W]ord"};
-								};
-								"<leader>fh" = {
-									action = "help_tags";
-									options = { desc = "[F]ind [H]elp"};
-								};
-        };
-
-vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
-vim.keymap.set('n', '<leader>fD', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
-vim.keymap.set('n', '<leader>fd', telescope.extensions.zoxide.list, { desc = '[F]ind by [D]irectory' })
-vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]ind [R]esume' })
-vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
-vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<C-p>', project_files, { desc = 'Find project files' })
+		  "<C-p>" = {
+			action = "git_files";
+			options = { desc = "Find project files";};
+		  };
+		  "<leader>fh" = {
+			action = "help_tags";
+			options = { desc = "[F]ind [H]elp";};
+		  };
+		  "<leader>fk" = {
+			action = "keymaps";
+			options = { desc = "[F]ind [K]eymaps";};
+		  };
+		  "<leader>ff" = {
+			action = "find_files";
+			options = { desc = "[F]ind [F]iles";};
+		  };
+		  "<leader>fs" = {
+			action = "builtin";
+			options = { desc = "[F]ind [S]elect Telescope";};
+		  };
+		  "<leader>fw" = {
+			action = "grep_string";
+			options = { desc = "[F]ind current [W]ord";};
+		  };
+		  "<leader>fg" = {
+			action = "live_grep";
+			options = { desc = "[F]ind by [G]rep";};
+		  };
+		  "<leader>fD" = {
+			action = "diagnostics";
+			options = { desc = "[F]ind [D]iagnostics";};
+		  };
+		  "<leader>fd" = {
+			action = "zoxide.list";
+			options = { desc = "[F]ind by [D]irectory";};
+		  };
+		  "<leader>fr" = { action = "resume"; options = { desc = "[F]ind [R]esume"}};
+		  "<leader>f." = { action = "oldfiles"; options = { desc = "[F]ind Recent Files (\".\" for repeat)"}};
+		  "<leader><leader>" = { action = "buffers"; options = { desc = "[F]ind [B]uffers";}};
+		};
 
 
         settings = {
