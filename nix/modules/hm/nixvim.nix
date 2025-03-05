935ca00bf60e47273b9c50221d3d9f44f70f6547
repparw <ubritos@gmail.com>
2025-03-05 20:24:03 -0300

    Autocommit

--- nix/modules/hm/nixvim.nix
+++ nix/modules/hm/nixvim.nix
@@ -51,47 +51,79 @@
       telescope = {
         enable = true;
         keymaps = {
-		  "<C-p>" = {
-			action = "git_files";
-			options = { desc = "Find project files";};
-		  };
-		  "<leader>fh" = {
-			action = "help_tags";
-			options = { desc = "[F]ind [H]elp";};
-		  };
-		  "<leader>fk" = {
-			action = "keymaps";
-			options = { desc = "[F]ind [K]eymaps";};
-		  };
-		  "<leader>ff" = {
-			action = "find_files";
-			options = { desc = "[F]ind [F]iles";};
-		  };
-		  "<leader>fs" = {
-			action = "builtin";
-			options = { desc = "[F]ind [S]elect Telescope";};
-		  };
-		  "<leader>fw" = {
-			action = "grep_string";
-			options = { desc = "[F]ind current [W]ord";};
-		  };
-		  "<leader>fg" = {
-			action = "live_grep";
-			options = { desc = "[F]ind by [G]rep";};
-		  };
-		  "<leader>fD" = {
-			action = "diagnostics";
-			options = { desc = "[F]ind [D]iagnostics";};
-		  };
-		  "<leader>fd" = {
-			action = "zoxide.list";
-			options = { desc = "[F]ind by [D]irectory";};
-		  };
-		  "<leader>fr" = { action = "resume"; options = { desc = "[F]ind [R]esume"}};
-		  "<leader>f." = { action = "oldfiles"; options = { desc = "[F]ind Recent Files (\".\" for repeat)"}};
-		  "<leader><leader>" = { action = "buffers"; options = { desc = "[F]ind [B]uffers";}};
-		};
-
+          "<C-p>" = {
+            action = "git_files";
+            options = {
+              desc = "Find project files";
+            };
+          };
+          "<leader>fh" = {
+            action = "help_tags";
+            options = {
+              desc = "[F]ind [H]elp";
+            };
+          };
+          "<leader>fk" = {
+            action = "keymaps";
+            options = {
+              desc = "[F]ind [K]eymaps";
+            };
+          };
+          "<leader>ff" = {
+            action = "find_files";
+            options = {
+              desc = "[F]ind [F]iles";
+            };
+          };
+          "<leader>fs" = {
+            action = "builtin";
+            options = {
+              desc = "[F]ind [S]elect Telescope";
+            };
+          };
+          "<leader>fw" = {
+            action = "grep_string";
+            options = {
+              desc = "[F]ind current [W]ord";
+            };
+          };
+          "<leader>fg" = {
+            action = "live_grep";
+            options = {
+              desc = "[F]ind by [G]rep";
+            };
+          };
+          "<leader>fD" = {
+            action = "diagnostics";
+            options = {
+              desc = "[F]ind [D]iagnostics";
+            };
+          };
+          "<leader>fd" = {
+            action = "zoxide.list";
+            options = {
+              desc = "[F]ind by [D]irectory";
+            };
+          };
+          "<leader>fr" = {
+            action = "resume";
+            options = {
+              desc = "[F]ind [R]esume";
+            };
+          };
+          "<leader>f." = {
+            action = "oldfiles";
+            options = {
+              desc = "[F]ind Recent Files (\".\" for repeat)";
+            };
+          };
+          "<leader><leader>" = {
+            action = "buffers";
+            options = {
+              desc = "[F]ind [B]uffers";
+            };
+          };
+        };
 
         settings = {
 
