09b30745fbae7f1ad8b71af28661c655a024aa25
repparw <ubritos@gmail.com>
2025-03-05 22:08:02 -0300

    Autocommit

--- nix/modules/hm/nixvim.nix
+++ nix/modules/hm/nixvim.nix
@@ -115,6 +115,32 @@
       cmp = {
         enable = true;
         settings = {
+          snippet.expand = ''
+            		function(args)
+                  vim.snippet.expand(args.body)
+                end
+          '';
+          mapping = {
+            "<C-n>" = "cmp.mapping.select_next_item()";
+            "<C-p>" = "cmp.mapping.select_prev_item()";
+            "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<C-y>" = "cmp.mapping.confirm { select = true }";
+            "<C-Space>" = "cmp.mapping.complete {}";
+            "<C-l>" =
+              "cmp.mapping(function()
+      if luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      end
+    end, { 'i', 's' })";
+            "<C-h>" =
+              "cmp.mapping(function()
+      if luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      end
+    end, { 'i', 's' })";
+
+          };
           sources = [
             {
               name = "nvim_lsp";
