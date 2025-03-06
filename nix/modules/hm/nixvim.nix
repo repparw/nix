b45897f22d6803806b6199810143358bb07b9bd7
repparw <ubritos@gmail.com>
2025-03-05 22:20:08 -0300

    Autocommit

--- nix/modules/hm/nixvim.nix
+++ nix/modules/hm/nixvim.nix
@@ -49,8 +49,22 @@
     plugins = {
       which-key.enable = true;
 
-      noice.enable = true;
-
+      noice = {
+        enable = true;
+        settings.presets = {
+          bottom_search = true;
+          command_palette = true;
+          long_message_to_split = true;
+          inc_rename = false;
+          lsp_doc_border = false;
+        };
+        views.cmdline_popup = {
+          position = {
+            row = "40%";
+            col = "50%";
+          };
+        };
+      };
       obsidian = {
         enable = true;
         settings = {
