d36807c777f136b17b02e879462eef39db8565e7
repparw <ubritos@gmail.com>
2025-03-05 21:08:04 -0300

    Autocommit

--- nix/modules/hm/nixvim.nix
+++ nix/modules/hm/nixvim.nix
@@ -104,14 +104,12 @@
         enable = true;
         settings = {
           sections = {
-            lualine_x = {
-			      {
-        "require('noice').api.statusline.mode.get";
-        cond = require('noice').api.statusline.mode.has;
-        color = { fg = "#ff9e64" };
-      };
+            lualine_x = [ {
+              "require('noice').api.statusline.mode.get"
+              cond = "require('noice').api.statusline.mode.has";
+              "color = { fg = # ff9e64 }"
+            }];
 
-            };
           };
         };
       };
