973010d31436e4775ef62455eaa424981cfb2e2d
repparw <ubritos@gmail.com>
2025-03-07 14:00:00 -0300

    Autocommit

--- nix/modules/hm/nixvim.nix
+++ nix/modules/hm/nixvim.nix
@@ -196,10 +196,12 @@
           };
         };
       };
-      treesitter.settings = {
-        highlight = {
-          enable = true;
-          disable = "function(lang, buf)
+      treesitter = {
+        enable = true;
+        settings = {
+          highlight = {
+            enable = true;
+            disable = "function(lang, buf)
       local max_filesize = 100 * 1024 -- 100 KB
       local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
       if ok and stats and stats.size > max_filesize then
@@ -207,6 +209,7 @@
       end
     end
 ";
+          };
         };
       };
       lualine = {
