cedebcaa4973464059b14fa52f31cc92bf3f6b3f
repparw <ubritos@gmail.com>
2025-03-08 16:40:06 -0300

    Autocommit

--- nix/modules/nixos/dlsuite.nix
+++ nix/modules/nixos/dlsuite.nix
@@ -390,8 +390,8 @@
     "vikunja" = {
       image = "docker.io/vikunja/vikunja:latest";
       environment = {
-        "TZ" = "America/Argentina/Buenos_Aires";
-        "VIKUNJA_SERVICE_PUBLICURL" = "https://todo.repparw.me";
+        "VIKUNJA_SERVICE_TIMEZONE" = "America/Argentina/Buenos_Aires";
+        #"VIKUNJA_SERVICE_PUBLICURL" = "https://todo.repparw.me";
         "VIKUNJA_DATABASE_HOST" = "vikunjadb";
         "VIKUNJA_DATABASE_PASSWORD" = "vikunja";
         "VIKUNJA_DATABASE_TYPE" = "postgres";
