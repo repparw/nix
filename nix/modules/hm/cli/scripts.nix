6a70eb3ed0867e92285d26db470302540761cb89
repparw <ubritos@gmail.com>
2025-03-10 13:56:06 -0300

    Autocommit

--- nix/modules/hm/cli/scripts.nix
+++ nix/modules/hm/cli/scripts.nix
@@ -1,6 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 {
 
-  pkgs.writeShellApplication = { };
+  obsinvim =
+    with pkgs;
+    writeShellApplication {
+      name = "";
+      runtimeInputs = [ ];
+      text = ''
+        cd /home/repparw/Documents/obsidian; nvim .
+      '';
+    };
+
+  git-autocommit =
+    with pkgs;
+    writeShellApplication {
+      name = "";
+      runtimeInputs = [ ];
+      text = '''';
+    };
 
 }
