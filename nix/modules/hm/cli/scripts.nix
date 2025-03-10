2936a9e3cbb59bd3d48060435ffbe35120faa2cc
repparw <ubritos@gmail.com>
2025-03-10 14:00:06 -0300

    Autocommit

--- nix/modules/hm/cli/scripts.nix
+++ nix/modules/hm/cli/scripts.nix
@@ -4,8 +4,8 @@
   obsinvim =
     with pkgs;
     writeShellApplication {
-      name = "";
-      runtimeInputs = [ ];
+      name = "obsinvim";
+      runtimeInputs = [ nvim ];
       text = ''
         cd /home/repparw/Documents/obsidian; nvim .
       '';
@@ -14,9 +14,16 @@
   git-autocommit =
     with pkgs;
     writeShellApplication {
-      name = "";
+      name = "git-autocommit";
       runtimeInputs = [ ];
-      text = '''';
+      text = ''
+        DIR=${"1:-/home/repparw/.dotfiles"}
+        cd "$DIR" || exit 1
+        git add -A
+        git commit -m "Autocommit"
+        git pull --rebase
+        git push
+      '';
     };
 
 }
