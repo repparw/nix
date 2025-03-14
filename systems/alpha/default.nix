bf1d10196f7914d322824a4ba45356a0b112a378
repparw <ubritos@gmail.com>
2025-03-14 18:44:05 -0300

    Autocommit

--- systems/alpha/default.nix
+++ systems/alpha/default.nix
@@ -42,14 +42,6 @@
 
   programs.adb.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   #### FSTAB
 
   fileSystems."/mnt/hdd" = {
