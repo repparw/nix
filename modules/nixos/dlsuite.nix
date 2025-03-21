6ef67985d4c9fac99411bb1109ec290909576b57
repparw <ubritos@gmail.com>
2025-03-21 14:00:00 -0300

    Autocommit

--- modules/nixos/dlsuite.nix
+++ modules/nixos/dlsuite.nix
@@ -42,6 +42,15 @@ in {
   };
 
   config = mkIf cfg.enable {
+    networking.firewall.interfaces = let
+      matchAll =
+        if !config.networking.nftables.enable
+        then "podman+"
+        else "podman*";
+    in {
+      "${matchAll}".allowedUDPPorts = [53];
+    };
+
     virtualisation = {
       podman = {
         enable = true;
