bde453a44e213b67d96ff035624b8410ac8a3937
repparw <ubritos@gmail.com>
2025-03-21 14:28:00 -0300

    Autocommit

--- modules/nixos/dlsuite.nix
+++ modules/nixos/dlsuite.nix
@@ -51,11 +51,16 @@ in {
       "${matchAll}".allowedUDPPorts = [53];
     };
 
-    users.users.dlsuite = {
-      isNormalUser = true;
-      linger = true;
-      description = "repparw";
-      users.users.dlsuite.group = "dlsuite";
+    users = {
+      users.dlsuite = {
+        isNormalUser = true;
+        linger = true;
+        group = "dlsuite";
+        uid = cfg.user;
+      };
+      groups.dlsuite = {
+        gid = cfg.group;
+      };
     };
 
     virtualisation = {
