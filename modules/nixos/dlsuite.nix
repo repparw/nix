b7d0ca3e1df80ca213e4937f7d8ecfdf45e31f42
repparw <ubritos@gmail.com>
2025-03-21 17:33:52 -0300

    Revert "Autocommit"
    This reverts commit 6ef67985d4c9fac99411bb1109ec290909576b57.

--- modules/nixos/dlsuite.nix
+++ modules/nixos/dlsuite.nix
@@ -42,15 +42,6 @@ in {
   };
 
   config = mkIf cfg.enable {
-    networking.firewall.interfaces = let
-      matchAll =
-        if !config.networking.nftables.enable
-        then "podman+"
-        else "podman*";
-    in {
-      "${matchAll}".allowedUDPPorts = [53];
-    };
-
     virtualisation = {
       podman = {
         enable = true;
