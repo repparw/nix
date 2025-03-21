0dbc8c4d062ebf188e9ecbbf2d73a9eb1ff87834
repparw <ubritos@gmail.com>
2025-03-21 19:28:22 -0300

    Autocommit

---
+++ modules/nixos/dlsuite/arr.nix
@@ -0,0 +1,68 @@
+{cfg}: {
+  "flaresolverr" = {
+    image = "docker.io/flaresolverr/flaresolverr:latest";
+    environment = {
+      "CAPTCHA_SOLVER" = "none";
+      "LOG_HTML" = "false";
+      "LOG_LEVEL" = "info";
+      "TZ" = cfg.timezone;
+    };
+  };
+  "prowlarr" = {
+    image = "docker.io/linuxserver/prowlarr:latest";
+    environment = {
+      "PGID" = cfg.group;
+      "PUID" = cfg.user;
+      "TZ" = cfg.timezone;
+    };
+    volumes = [
+      "${cfg.dataDir}/prowlarr:/config:rw,Z"
+    ];
+  };
+  "qbittorrent" = {
+    image = "docker.io/hotio/qbittorrent:latest";
+    environment = {
+      "PGID" = cfg.group;
+      "PUID" = cfg.user;
+      "TZ" = cfg.timezone;
+    };
+    volumes = [
+      "${cfg.dataDir}/data/torrents:/data/torrents:rw,z"
+      "${cfg.dataDir}/qbittorrent:/config:rw,Z"
+    ];
+    ports = [
+      "127.0.0.1:54536:54536/tcp"
+    ];
+  };
+  "radarr" = {
+    image = "docker.io/linuxserver/radarr:latest";
+    environment = {
+      "PGID" = cfg.group;
+      "PUID" = cfg.user;
+      "TZ" = cfg.timezone;
+    };
+    volumes = [
+      "${cfg.dataDir}/data/:/data:rw,z"
+      "${cfg.dataDir}/radarr:/config:rw,Z"
+    ];
+    dependsOn = [
+      "qbittorrent"
+    ];
+  };
+  "sonarr" = {
+    image = "docker.io/linuxserver/sonarr:latest";
+    environment = {
+      "PGID" = cfg.group;
+      "PUID" = cfg.user;
+      "TZ" = cfg.timezone;
+    };
+    volumes = [
+      "/dev/rtc:/dev/rtc:ro"
+      "${cfg.dataDir}/data:/data:rw,z"
+      "${cfg.dataDir}/sonarr:/config:rw,Z"
+    ];
+    dependsOn = [
+      "qbittorrent"
+    ];
+  };
+}
---
+++ modules/nixos/dlsuite/diun.nix
@@ -0,0 +1,17 @@
+{cfg}: {
+  "diun" = {
+    image = "docker.io/crazymax/diun:latest";
+    environment = {
+      "TZ" = cfg.timezone;
+      "DIUN_WATCH_WORKERS" = "20";
+      "DIUN_WATCH_SCHEDULE" = "@every 12h";
+      "DIUN_PROVIDERS_DOCKER" = "true";
+      "DIUN_PROVIDERS_DOCKER_WATCHBYDEFAULT" = "true";
+      "DIUN_NOTIF_DISCORD_WEBHOOKURLFILE" = "/data/discord-webhook-url";
+    };
+    volumes = [
+      "${cfg.dataDir}/diun:/data:rw,Z"
+      "/var/run/docker.sock:/var/run/docker.sock:ro"
+    ];
+  };
+}
