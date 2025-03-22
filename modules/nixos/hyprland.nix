d118562419bf33a13c082db40bcc164d60dc981c
repparw <ubritos@gmail.com>
2025-03-22 20:12:39 -0300

    Autocommit

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -9,25 +9,27 @@
   ];
 
   services.displayManager = {
-  autoLogin = {
-    enable = true;
-    user = "repparw";
-  };
-  sddm = {
-  		enable = true;
-  };
-
-  services.greetd = {
-    enable = true;
-    vt = 1;
-    settings = let
-      session = {
-        command = "${pkgs.uwsm}/bin/uwsm start hyprland";
-        user = "repparw";
-      };
-    in {
-      initial_session = session;
-      default_session = session;
+    autoLogin = {
+      enable = true;
+      user = "repparw";
+    };
+    sddm = {
+      enable = true;
+      wayland.enable = true;
     };
   };
+
+  #services.greetd = {
+  #  enable = true;
+  #  vt = 1;
+  #  settings = let
+  #    session = {
+  #      command = "${pkgs.uwsm}/bin/uwsm start hyprland";
+  #      user = "repparw";
+  #    };
+  #  in {
+  #    initial_session = session;
+  #    default_session = session;
+  #  };
+  #};
 }
