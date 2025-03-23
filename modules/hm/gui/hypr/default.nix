56334214a4509b083ab8253374fa3d32a2becc9a
repparw <ubritos@gmail.com>
2025-03-23 19:08:02 -0300

    Autocommit

--- modules/hm/gui/hypr/default.nix
+++ modules/hm/gui/hypr/default.nix
@@ -12,6 +12,7 @@
   config = lib.mkIf osConfig.programs.hyprland.enable {
     wayland.windowManager.hyprland = {
       enable = true;
+      systemd.enable = true;
       plugins = with pkgs.hyprlandPlugins; [
         hyprsplit
       ];
