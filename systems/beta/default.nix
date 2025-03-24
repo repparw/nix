b9d1a326b98ab9abd33d31c0ebdbf0385228dfb1
repparw <ubritos@gmail.com>
2025-03-24 16:40:05 -0300

    Autocommit

--- systems/beta/default.nix
+++ systems/beta/default.nix
@@ -1,16 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {pkgs, ...}: {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
 
   networking.hostName = "beta"; # Define your hostname.
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   virtualisation.docker = {
     enable = true;
