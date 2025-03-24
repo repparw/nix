84fe8fbe164b5ea59e3dc3bbc5977b3b26ccd3ef
repparw <ubritos@gmail.com>
2025-03-24 16:32:04 -0300

    Autocommit

--- modules/hm/gui/spotifyd.nix
+++ modules/hm/gui/spotifyd.nix
@@ -1,34 +1,40 @@
-{...}: {
-  services.spotifyd = {
-    settings = {
-      global = {
-        username = "2ksy00sfypgevoabx2128ia4g";
-        use_mpris = true;
+{
+  lib,
+  config,
+  ...
+}: {
+  config = lib.mkIf config.modules.gui.enable {
+    services.spotifyd = {
+      settings = {
+        global = {
+          username = "2ksy00sfypgevoabx2128ia4g";
+          use_mpris = true;
 
-        dbus_type = "session";
-        backend = "pulseaudio";
-        audio_format = "S16";
+          dbus_type = "session";
+          backend = "pulseaudio";
+          audio_format = "S16";
 
-        device_name = "spotifyd";
+          device_name = "spotifyd";
 
-        bitrate = 320;
+          bitrate = 320;
 
-        cache_path = "/home/repparw/.cache/spotifyd";
+          cache_path = "/home/repparw/.cache/spotifyd";
 
-        max_cache_size = 5000000000;
+          max_cache_size = 5000000000;
 
-        # NOTE: This variable's type will change in v0.4, to a number (instead of string)
-        initial_volume = "45";
+          # NOTE: This variable's type will change in v0.4, to a number (instead of string)
+          initial_volume = "45";
 
-        volume_normalisation = false;
+          volume_normalisation = false;
 
-        normalisation_pregain = 3;
+          normalisation_pregain = 3;
 
-        autoplay = true;
+          autoplay = true;
 
-        zeroconf_port = 1234;
+          zeroconf_port = 1234;
 
-        device_type = "speaker";
+          device_type = "speaker";
+        };
       };
     };
   };
