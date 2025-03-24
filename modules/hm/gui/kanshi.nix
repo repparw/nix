b9458e3eb28c910f68226f23111ca83f5adb1c93
repparw <ubritos@gmail.com>
2025-03-24 16:08:04 -0300

    Autocommit

--- modules/hm/gui/kanshi.nix
+++ modules/hm/gui/kanshi.nix
@@ -1,31 +1,43 @@
-{...}: {
-  services.kanshi = {
-    enable = true;
-    systemdTarget = "graphical-session.target";
-    settings = [
-      {
-        profile.name = "docked";
-        profile.outputs = [
-          {
-            criteria = "eDP-1";
-            status = "disable";
-          }
-          {
-            criteria = "DP-3";
-            status = "enable";
-            mode = "1920x1080@60";
-          }
-        ];
-      }
-      {
-        profile.name = "undocked";
-        profile.outputs = [
-          {
-            criteria = "eDP-1";
-            status = "enable";
-          }
-        ];
-      }
-    ];
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.modules.kanshi;
+in {
+  options.modules.kanshi = {
+    enable = lib.mkEnableOption "Kanshi display management service";
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.kanshi = {
+      enable = true;
+      systemdTarget = "graphical-session.target";
+      settings = [
+        {
+          profile.name = "docked";
+          profile.outputs = [
+            {
+              criteria = "eDP-1";
+              status = "disable";
+            }
+            {
+              criteria = "DP-3";
+              status = "enable";
+              mode = "1920x1080@60";
+            }
+          ];
+        }
+        {
+          profile.name = "undocked";
+          profile.outputs = [
+            {
+              criteria = "eDP-1";
+              status = "enable";
+            }
+          ];
+        }
+      ];
+    };
   };
 }
