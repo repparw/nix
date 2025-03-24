503ba29833411663404f06328a2b4495acc2da69
repparw <ubritos@gmail.com>
2025-03-24 19:16:04 -0300

    Autocommit

---
+++ systems/iso/default.nix
@@ -0,0 +1,13 @@
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+  ];
+
+  isoImage.squashfsCompression = "gzip -Xcompression-level 1";
+}
