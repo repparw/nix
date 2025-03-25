5c21adb72b714b4a427dc061af6f9faa1588b009
repparw <ubritos@gmail.com>
2025-03-24 21:48:04 -0300

    Autocommit

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,3 +1,12 @@
-[
-  (import ./mpv.nix)
-]
+{...}: {
+  modifications = final: prev: {
+    mpv = prev.mpv.override {
+      scripts = with final.mpvScripts; [
+        mpv-webm
+        mpris
+        quality-menu
+        sponsorblock-minimal
+      ];
+    };
+  };
+}
