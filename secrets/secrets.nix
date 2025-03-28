6c20cbaea95a3e71d66e9400f2b8ca5a26a5906f
repparw <ubritos@gmail.com>
2025-03-28 12:54:46 -0300

    add beta to secret

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,18 +1,20 @@
 let
   repparw-alpha = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPFzKXBKbNZ+jr06UNKj0MHIzYw54CMP6suD8iTd7CxH";
-  users = [repparw-alpha];
+  repparw-beta = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN6UbXeSlW/2jkIU9mQIN5xWElnFbA9tw0BfT072WXgR";
+  users = [
+    repparw-alpha
+    repparw-beta
+  ];
 
   alpha = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIrytCarAMw1Q2QfgBQd1jgtWsLdetbFXepFEcxwKOBI";
   systems = [
     alpha
   ];
-in {
+in
+{
   "github.age".publicKeys = [
     repparw-alpha
-    alpha
-  ];
-  "vikunja.age".publicKeys = [
-    repparw-alpha
+    repparw-beta
     alpha
   ];
 }
