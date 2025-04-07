f83c7aee885688a8acad5442949fb528c31c1e8e
repparw <ubritos@gmail.com>
2025-04-07 14:04:36 -0300

    Autocommit

--- modules/nixos/dlsuite/ntfy.nix
+++ modules/nixos/dlsuite/ntfy.nix
@@ -8,9 +8,9 @@
       "NTFY_CACHE_FILE" = "${cfg.dataDir}/ntfy/cache.db";
       "NTFY_AUTH_FILE" = "${cfg.dataDir}/ntfy/auth.db";
       "NTFY_AUTH_DEFAULT_ACCESS" = "deny-all";
-      "NTFY_BEHIND_PROXY" = true;
+      "NTFY_BEHIND_PROXY" = "true";
       "NTFY_ATTACHMENT_CACHE_DIR" = "${cfg.dataDir}/ntfy/attachments";
-      "NTFY_ENABLE_LOGIN" = true;
+      "NTFY_ENABLE_LOGIN" = "true";
     };
     user = "${cfg.user}:${cfg.group}";
   };
