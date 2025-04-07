9553665105477d168b0633f67a2cc035ec411b62
repparw <ubritos@gmail.com>
2025-04-07 17:12:40 -0300

    Autocommit

--- modules/nixos/dlsuite/diun.nix
+++ modules/nixos/dlsuite/diun.nix
@@ -7,7 +7,7 @@
       "DIUN_WATCH_SCHEDULE" = "@every 12h";
       "DIUN_PROVIDERS_DOCKER" = "true";
       "DIUN_PROVIDERS_DOCKER_WATCHBYDEFAULT" = "true";
-      "DIUN_NOTIF_NTFY_TOKENFILE" = "/data/ntfy_token"; # TODO move to agenix secret
+      "DIUN_NOTIF_NTFY_TOKENFILE" = config.age.secrets.diun-ntfy.path;
       "DIUN_NOTIF_NTFY_TOPIC" = "diun";
       "DIUN_NOTIF_NTFY_ENDPOINT" = "https://ntfy.${cfg.domain}";
     };
