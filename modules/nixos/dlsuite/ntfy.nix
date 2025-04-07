5b2dcd68007621c0c08e600456fc6a22e54d0193
repparw <ubritos@gmail.com>
2025-04-07 13:56:40 -0300

    Autocommit

--- modules/nixos/dlsuite/ntfy.nix
+++ modules/nixos/dlsuite/ntfy.nix
@@ -4,6 +4,13 @@
     cmd = ["serve"];
     environment = {
       "TZ" = cfg.timezone;
+      "NTFY_BASE_URL" = "https://ntfy.${cfg.domain}";
+      "NTFY_CACHE_FILE" = "${cfg.dataDir}/ntfy/cache.db";
+      "NTFY_AUTH_FILE" = "${cfg.dataDir}/ntfy/auth.db";
+      "NTFY_AUTH_DEFAULT_ACCESS" = "deny-all";
+      "NTFY_BEHIND_PROXY" = true;
+      "NTFY_ATTACHMENT_CACHE_DIR" = "${cfg.dataDir}/ntfy/attachments";
+      "NTFY_ENABLE_LOGIN" = true;
     };
     user = "${cfg.user}:${cfg.group}";
     volumes = [
