a6642e8b3736301dd8c3150fccd016bc61e38832
repparw <ubritos@gmail.com>
2025-04-07 16:48:40 -0300

    Autocommit

--- modules/nixos/dlsuite/diun.nix
+++ modules/nixos/dlsuite/diun.nix
@@ -7,7 +7,9 @@
       "DIUN_WATCH_SCHEDULE" = "@every 12h";
       "DIUN_PROVIDERS_DOCKER" = "true";
       "DIUN_PROVIDERS_DOCKER_WATCHBYDEFAULT" = "true";
-      "DIUN_NOTIF_DISCORD_WEBHOOKURLFILE" = "/data/discord-webhook-url";
+      "DIUN_NOTIF_NTFY_TOKENFILE" = "/data/ntfy_token"; # TODO move to agenix secret
+      "DIUN_NOTIF_NTFY_TOPIC" = "diun";
+      "DIUN_NOTIF_NTFY_ENDPOINT" = "https://ntfy.${cfg.domain}";
     };
     volumes = [
       "${cfg.dataDir}/diun:/data:rw,Z"
