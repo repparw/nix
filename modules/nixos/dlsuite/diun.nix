bfd6c4b1768faea7718e9a98f32f4491b16dfaf8
repparw <ubritos@gmail.com>
2025-04-07 17:20:40 -0300

    Autocommit

--- modules/nixos/dlsuite/diun.nix
+++ modules/nixos/dlsuite/diun.nix
@@ -1,4 +1,7 @@
-{cfg}: {
+{
+  config,
+  cfg,
+}: {
   "diun" = {
     image = "docker.io/crazymax/diun:latest";
     environment = {
@@ -7,7 +10,7 @@
       "DIUN_WATCH_SCHEDULE" = "@every 12h";
       "DIUN_PROVIDERS_DOCKER" = "true";
       "DIUN_PROVIDERS_DOCKER_WATCHBYDEFAULT" = "true";
-      "DIUN_NOTIF_NTFY_TOKENFILE" = ”${config.age.secrets.diun-ntfy.path}";
+      "DIUN_NOTIF_NTFY_TOKENFILE" = "/data/diun-ntfy";
       "DIUN_NOTIF_NTFY_TOPIC" = "diun";
       "DIUN_NOTIF_NTFY_ENDPOINT" = "https://ntfy.${cfg.domain}";
     };
