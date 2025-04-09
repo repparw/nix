fbe041c52eeae37736d1661873cb89ffa3f380cf
repparw <ubritos@gmail.com>
2025-04-09 16:16:40 -0300

    Autocommit

--- modules/hm/gui/hypr/default.nix
+++ modules/hm/gui/hypr/default.nix
@@ -59,7 +59,7 @@
         "$showkeys" = "wshowkeys -a bottom -m 108 -b 00000066";
         "$screenshot" = "hyprshot -o $XDG_SCREENSHOTS_DIR -m";
         "$menu" = "killall tofi-drun || tofi-drun";
-        "$emojimenu" = "killall tofi || BEMOJI_PICKER_CMD=${lib.getExe pkgs.tofi} bemoji -n";
+        #"$emojimenu" = "killall tofi || BEMOJI_PICKER_CMD=${lib.getExe pkgs.tofi} bemoji -n";
         "$cmdmenu" = "killall tofi-run || tofi-run | xargs hyprctl dispatch exec --";
 
         "$showlayout" = "hdrop feh -g 774x275 /home/repparw/git/kbd/layout.png";
