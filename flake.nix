8e4d53206200257b8e4297071a89dea035fe375e
repparw <ubritos@gmail.com>
2025-04-09 17:12:16 -0300

    Autocommit

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     agenix.url = "github:ryantm/agenix";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
-    home-manager.url = "github:nix-community/home-manager"; # Branches for stable, master follows unstable
+    home-manager.url = "github:nix-community/home-manager";
     nix-index-database.url = "github:nix-community/nix-index-database";
     nur.url = "github:nix-community/NUR";
   };
