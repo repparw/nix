0ecf3520d8acf0f7831f036881fe04d46dc011b5
repparw <ubritos@gmail.com>
2025-04-09 13:24:16 -0300

    Autocommit

--- modules/nixos/dlsuite/diun.nix
+++ modules/nixos/dlsuite/diun.nix
@@ -7,8 +7,9 @@
       "DIUN_WATCH_SCHEDULE" = "@every 12h";
       "DIUN_PROVIDERS_DOCKER" = "true";
       "DIUN_PROVIDERS_DOCKER_WATCHBYDEFAULT" = "true";
-      "DIUN_NOTIF_NTFY_ENDPOINTFILE" = "/data/diun-ntfy";
+      "DIUN_NOTIF_NTFY_TOKENFILE" = "/data/diun-ntfy";
       "DIUN_NOTIF_NTFY_TOPIC" = "diun";
+      "DIUN_NOTIF_NTFY_ENDPOINT" = "https://ntfy.${cfg.domain}";
     };
     volumes = [
       "${cfg.dataDir}/diun:/data:rw,Z"
