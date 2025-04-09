71758653e7dc8f137b1ccbfa46bce0c0055e71a5
repparw <ubritos@gmail.com>
2025-04-09 12:04:13 -0300

    Autocommit

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  repparw-alpha = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPFzKXBKbNZ+jr06UNKj0MHIzYw54CMP6suD8iTd7CxH";
+  repparw-alpha = "";
   repparw-beta = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN6UbXeSlW/2jkIU9mQIN5xWElnFbA9tw0BfT072WXgR";
   users = [
     repparw-alpha
