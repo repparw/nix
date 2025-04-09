7621a38a61c53d6d8ac79c349a1ac2adb0bc7dc0
repparw <ubritos@gmail.com>
2025-04-09 12:52:17 -0300

    Autocommit

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,25 +1,12 @@
 let
-  repparw-alpha = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMjQf2hK5Ir0hhdx2VYj6EXU/ZmSHMPZ5u5VzCM77LKa";
+  repparw-alpha = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMjQf2hK5Ir0hhdx2VYj6EXU/ZmSHMPZ5u5VzCM77LKa ubritos@gmail.com";
   repparw-beta = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN6UbXeSlW/2jkIU9mQIN5xWElnFbA9tw0BfT072WXgR";
   users = [
     repparw-alpha
     repparw-beta
   ];
-
-  alpha = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIrytCarAMw1Q2QfgBQd1jgtWsLdetbFXepFEcxwKOBI";
-  systems = [
-    alpha
-  ];
 in {
-  "github.age".publicKeys = [
-    repparw-alpha
-    repparw-beta
-    alpha
-  ];
+  "github.age".publicKeys = users;
 
-  "diun-ntfy.age".publicKeys = [
-    repparw-alpha
-    repparw-beta
-    alpha
-  ];
+  "diun-ntfy.age".publicKeys = users;
 }
