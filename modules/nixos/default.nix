0b1aba5354ef0c36b95790b3f4d12ebdcaa3562b
repparw <ubritos@gmail.com>
2025-04-10 12:28:27 -0300

    Autocommit

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -20,6 +20,30 @@
     efi.canTouchEfiVariables = true;
   };
 
+  nix = {
+    nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+
+    #extraOptions = '' !include ${config.age.secrets.accessTokens.path} '';
+
+    settings = {
+      trusted-users = [
+        "root"
+        "repparw"
+      ];
+
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+      experimental-features = "nix-command flakes";
+    };
+
+    optimise.automatic = true;
+  };
+
   time.timeZone = "America/Argentina/Buenos_Aires";
 
   i18n = {
