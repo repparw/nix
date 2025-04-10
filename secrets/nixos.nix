c4e28256eabe565167ebdd41bc375fe215f9b3ff
repparw <ubritos@gmail.com>
2025-04-10 11:56:29 -0300

    Autocommit

--- secrets/nixos.nix
+++ secrets/nixos.nix
@@ -1,26 +1,29 @@
-{...}: {
-  age.secrets = {
-    accessTokens = {
-      file = ../secrets/access-tokens.age;
-      owner = "repparw";
-    };
-	// (if lib.mkIf modules.timers.enable then {
-	  rcloneDrive = {
-		file = ../../secrets/rclone-drive.age;
-		owner = "repparw";
-	  };
-	  rcloneCrypt = {
-		file = ../../secrets/rclone-crypt.age;
-		owner = "repparw";
-	  };
-	  rcloneDropbox = {
-		file = ../../secrets/rclone-dropbox.age;
-		owner = "repparw";
-	  };
-    };
-  });
+{
+  lib,
+  config,
+  ...
+}: {
+  age.secrets =
+    {
+      accessTokens = {
+        file = ../secrets/access-tokens.age;
+        owner = "repparw";
+      };
+    }
+    // (lib.mkIf config.modules.timers.enable {
+      rcloneDrive = {
+        file = ../../secrets/rclone-drive.age;
+        owner = "repparw";
+      };
+      rcloneCrypt = {
+        file = ../../secrets/rclone-crypt.age;
+        owner = "repparw";
+      };
+      rcloneDropbox = {
+        file = ../../secrets/rclone-dropbox.age;
+        owner = "repparw";
+      };
+    });
 
   age.identityPaths = ["/home/repparw/.ssh/id_ed25519"];
-	  };
-
 }
