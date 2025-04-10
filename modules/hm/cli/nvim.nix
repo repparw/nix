058e829e86b0652e018a7112d2058dfb6999efa2
repparw <ubritos@gmail.com>
2025-04-10 14:08:56 -0300

    Autocommit

---
+++ modules/hm/cli/nvim.nix
@@ -0,0 +1,20 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    stylua
+    lua-language-server
+
+    biome
+    nodePackages.prettier
+
+    beautysh
+
+    marksman
+
+    nixd
+    alejandra
+
+    typescript-language-server
+
+    inputs.nixvim-config.packages.${system}.default
+  ];
+}
