9c67ee4ada57aac9dcfd6504e1354e5f67406dea
repparw <ubritos@gmail.com>
2025-04-10 11:12:22 -0300

    Autocommit

--- systems/common.nix
+++ systems/common.nix
@@ -49,7 +49,9 @@
 
   age.identityPaths = ["/home/repparw/.ssh/id_ed25519"];
 
-  nix.extraOptions = ''!include ${config.age.secrets.accessTokens.path}'';
+  nix.extraOptions = ''
+    !include ${config.age.secrets.accessTokens.path}
+  '';
 
   nix.settings = {
     trusted-users = [
