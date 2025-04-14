71c32f1620b4a36c9a4bad8e57346c3da23986f0
repparw <ubritos@gmail.com>
2025-04-14 14:12:02 -0300

    Autocommit

--- modules/nixos/podman.nix
+++ modules/nixos/podman.nix
@@ -31,6 +31,7 @@ in {
   config = mkIf cfg.enable {
     virtualisation = {
       podman = {
+        enable = true;
         autoPrune.enable = true;
         defaultNetwork.settings.dns_enabled = true;
         dockerCompat = true;
