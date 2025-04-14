a9e47ea15f47e3f1eca58c7888ba4a12fb3fa849
repparw <ubritos@gmail.com>
2025-04-14 18:16:00 -0300

    Autocommit

--- modules/nixos/podman.nix
+++ modules/nixos/podman.nix
@@ -83,9 +83,6 @@ in {
     };
 
     boot.kernel.sysctl = {"net.ipv4.ip_unprivileged_port_start" = 0;};
-    networking.firewall.enable = true;
-    networking.firewall.allowedTCPPorts = [80 443];
-    networking.firewall.allowedUDPPorts = [80 443];
 
     networking.firewall.trustedInterfaces = ["podman*"];
   };
