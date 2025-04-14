06e26a4ccbfbc3a32fc0f47b6a958dfb715b31ff
repparw <ubritos@gmail.com>
2025-04-14 16:36:02 -0300

    Autocommit

--- modules/nixos/podman.nix
+++ modules/nixos/podman.nix
@@ -35,6 +35,42 @@ in {
         autoPrune.enable = true;
         defaultNetwork.settings.dns_enabled = true;
       };
+
+      oci-containers = {
+        backend = "podman";
+        containers = {
+          "swag" = {
+            image = "docker.io/linuxserver/swag:latest";
+            environment = {
+              "DNSPLUGIN" = "cloudflare";
+              "TZ" = cfg.timezone;
+              "SUBDOMAINS" = "wildcard";
+              "URL" = cfg.domain;
+              "VALIDATION" = "dns";
+            };
+            volumes = [
+              "${cfg.dataDir}/swag:/config:rw,Z"
+              "/home/repparw/src/homepage:/config/www:rw,Z"
+            ];
+            ports = [
+              "80:80/tcp"
+              "443:443/tcp"
+            ];
+            autoStart = true;
+          };
+
+          "ddclient" = {
+            image = "docker.io/linuxserver/ddclient:latest";
+            environment = {
+              "TZ" = cfg.timezone;
+            };
+            volumes = [
+              "${cfg.dataDir}/ddclient:/config:rw,Z"
+            ];
+            autoStart = true;
+          };
+        };
+      };
     };
 
     networking.firewall.trustedInterfaces = ["podman*"];
