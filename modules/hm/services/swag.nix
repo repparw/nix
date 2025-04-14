d9ba208a9779b072fff1e6ee1de463eec878ffe1
repparw <ubritos@gmail.com>
2025-04-14 15:36:08 -0300

    Autocommit

--- modules/hm/services/swag.nix
+++ modules/hm/services/swag.nix
@@ -13,14 +13,13 @@
       "/home/repparw/src/homepage:/config/www:rw,Z"
     ];
     ports = [
-      "127.0.0.1:8080:80/tcp"
-      "127.0.0.1:8443:443/tcp"
+      "127.0.0.1:80:80/tcp"
+      "127.0.0.1:443:443/tcp"
     ];
     extraPodmanArgs = [
       "--add-host=host.docker.internal:host-gateway"
     ];
     addCapabilities = [
-      "NET_ADMIN"
       "NET_BIND_SERVICE"
     ];
   };
--- modules/nixos/podman.nix
+++ modules/nixos/podman.nix
@@ -40,15 +40,6 @@ in {
     # Allow rootless to bind to ports < 1024 (>= 80)
     boot.kernel.sysctl = {"net.ipv4.ip_unprivileged_port_start" = 80;};
 
-    networking.firewall = {
-      allowedTCPPorts = [8080 8443]; # Ensure your new ports are allowed for Nginx
-      extraForwardRules = ''
-        # Redirect HTTP (port 80) to Nginx on port 8080
-        tcp dport 80 counter dnat to :8080
-
-        # Redirect HTTPS (port 443) to Nginx on port 8443
-        tcp dport 443 counter dnat to :8443
-      '';
-    };
+    networking.firewall.trustedInterfaces = ["podman*"];
   };
 }
