62df120f2cfbe992c6eee93d7a98ae5a2ddc628f
repparw <ubritos@gmail.com>
2025-04-29 11:30:09 -0300

    rm ls_colors

--- modules/hm/cli/zsh.nix
+++ modules/hm/cli/zsh.nix
@@ -47,10 +47,6 @@
         # history search on vi mode
         zvm_after_init_commands+=('bindkey -M vicmd "k" history-substring-search-up')
         zvm_after_init_commands+=('bindkey -M vicmd "j" history-substring-search-down')
-
-        export LS_COLORS="di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
-        ## Leave this here because omz overwrites this after .zprofile
-        zstyle ':completion:*' list-colors "di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
       ''
     ];
     shellAliases =
