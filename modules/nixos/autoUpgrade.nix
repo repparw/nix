36380f2c817df25fd72541b11cbc3fe719b0549e
repparw <ubritos@gmail.com>
2025-05-07 19:32:58 -0300

    fixup! replace all instances of ~/nix with self.outPath

--- modules/nixos/autoUpgrade.nix
+++ modules/nixos/autoUpgrade.nix
@@ -1,27 +1,40 @@
 {
   pkgs,
-  inputs,
-  lib,
+  config,
   ...
-}: {
-  system.autoUpgrade = {
-    enable = true;
-    flake = "github:repparw/nix";
-    dates = "02:00";
-  };
-
-  # Service to update flake lock file
-  systemd.services.flake-update = {
-    description = "Update Nix flake lock file";
-    path = with pkgs; [git];
-    serviceConfig = {
-      WorkingDirectory = inputs.self.outPath;
-      Type = "oneshot";
-      ExecStart = "${lib.getExe pkgs.nix} flake update --commit-lock-file";
+}: let
+  flakePath = "~/nix";
+in {
+  ## Update flake inputs daily
+  systemd.services = {
+    flake-update = {
+      unitConfig = {
+        Description = "Update flake inputs";
+        StartLimitIntervalSec = 300;
+        StartLimitBurst = 5;
+      };
+      serviceConfig = {
+        ExecStart = "${pkgs.nix}/bin/nix flake update --commit-lock-file ${flakePath}";
+        Restart = "on-failure";
+        RestartSec = "30";
+        Type = "oneshot"; # Ensure that it finishes before starting nixos-upgrade
+      };
     };
   };
+
+  system.autoUpgrade = {
+    enable = true;
+    flake = "${flakePath}#${config.networking.hostName}";
+    flags = [
+      "-L"
+    ];
+    dates = "04:40";
+    persistent = true;
+    randomizedDelaySec = "45min";
+  };
+
   systemd.services.nixos-upgrade = {
     after = ["flake-update.service"];
-    requires = ["flake-update.service"];
+    wants = ["flake-update.service"];
   };
 }
