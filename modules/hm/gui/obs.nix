d855097220a6f8f7d49c8369ac06b1d9676f7dbc
repparw <ubritos@gmail.com>
2025-05-18 18:08:08 -0300

    chore: change config if to lib.mkIf

--- modules/hm/gui/obs.nix
+++ modules/hm/gui/obs.nix
@@ -4,28 +4,25 @@
   osConfig,
   ...
 }: {
-  config =
-    if osConfig.programs.obs-studio.enable
-    then {
-      systemd.user.services."obs" = {
-        Unit = {
-          StartLimitIntervalSec = 60;
-          StartLimitBurst = 4;
-          After = ["graphical-session.target"];
-        };
-
-        Service = {
-          ExecStart = [
-            "${lib.getExe pkgs.obs-studio} --disable-shutdown-check --startreplaybuffer --minimize-to-tray"
-          ];
-          Restart = ["on-failure"];
-          RestartSec = 1;
-        };
+  config = lib.mkIf osConfig.programs.obs-studio.enable {
+    systemd.user.services."obs" = {
+      Unit = {
+        StartLimitIntervalSec = 60;
+        StartLimitBurst = 4;
+        After = ["graphical-session.target"];
       };
 
-      home.packages = with pkgs; [
-        obs-cmd
-      ];
-    }
-    else {};
+      Service = {
+        ExecStart = [
+          "${lib.getExe pkgs.obs-studio} --disable-shutdown-check --startreplaybuffer --minimize-to-tray"
+        ];
+        Restart = ["on-failure"];
+        RestartSec = 1;
+      };
+    };
+
+    home.packages = with pkgs; [
+      obs-cmd
+    ];
+  };
 }
