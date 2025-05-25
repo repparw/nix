5238d6e95e833a7dc721cd82d4b8df2f1c419043
repparw <ubritos@gmail.com>
2025-05-25 15:05:12 -0300

    nix: enable garbage collection

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -82,6 +82,12 @@
     };
 
     optimise.automatic = true;
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
   };
 
   time.timeZone = "America/Argentina/Buenos_Aires";
