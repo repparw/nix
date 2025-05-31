f95e7b01002ae7b4b1d138a242b769804ab14225
repparw <ubritos@gmail.com>
2025-05-31 18:27:38 -0300

    mpv: fix libplacebo regression, remove profiles

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,18 @@
 { inputs, ... }:
 {
   modifications = final: prev: {
+    mpv-unwrapped = prev.mpv-unwrapped.override {
+      libplacebo = final.libplacebo-mpv;
+    };
+    libplacebo-mpv = prev.libplacebo.overrideAttrs (old: {
+      src = prev.fetchFromGitLab {
+        domain = "code.videolan.org";
+        owner = "videolan";
+        repo = "libplacebo";
+        rev = "v7.349.0";
+        hash = "sha256-mIjQvc7SRjE1Orb2BkHK+K1TcRQvzj2oUOCUT4DzIuA=";
+      };
+    });
     mpv = prev.mpv.override {
       scripts = with prev.mpvScripts; [
         mpv-webm
