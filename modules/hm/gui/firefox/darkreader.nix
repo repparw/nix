35099e6429666ccdaff859a852c67370f2eea4b8
repparw <ubritos@gmail.com>
2025-07-04 13:58:47 -0300

    firefox: extensions.settings

---
+++ modules/hm/gui/firefox/darkreader.nix
@@ -0,0 +1,16 @@
+{
+  schemeVersion = 2;
+  enabled = true;
+  fetchNews = true;
+  enabledByDefault = true;
+  changeBrowserTheme = false;
+  syncSettings = true;
+  syncSitesFixes = true;
+  automation = {
+    enabled = false;
+  };
+  previewNewDesign = true;
+  previewNewestDesign = false;
+  enableForPDF = true;
+  detectDarkTheme = true;
+}
---
+++ modules/hm/gui/firefox/improvedtube.nix
@@ -0,0 +1,163 @@
+{
+  "add_scroll_to_top" = false;
+  "ads" = "all_videos";
+  "always_show_progress_bar" = false;
+  "ambient_lighting" = false;
+  "below_player_loop" = false;
+  "below_player_pip" = false;
+  "below_player_screenshot" = false;
+  "block_h264" = false;
+  "channel_compact_theme" = true;
+  "channel_default_tab" = "/videos";
+  "channel_hide_featured_content" = true;
+  "channel_trailer_autoplay" = false;
+  "channel_videos_count" = false;
+  "chapters" = false;
+  "collapse_of_subscription_sections" = false;
+  "columns" = false;
+  "comments" = "normal";
+  "comments_sidebar" = false;
+  "comments_sidebar_scrollbars" = false;
+  "comments_sidebar_simple" = false;
+  "compactSpacing" = false;
+  "description" = "normal";
+  "disable_thumbnail_playback" = false;
+  "duration_with_speed" = true;
+  "embeddedHideShare" = true;
+  "embeddedHideYoutubeLogo" = false;
+  "forced_theater_mode" = false;
+  "header_hide_country_code" = true;
+  "header_hide_logo" = false;
+  "header_hide_right_buttons" = false;
+  "header_improve_logo" = false;
+  "header_position" = "normal";
+  "header_transparent" = false;
+  "header_transparent_alternative" = false;
+  "hide_animated_thumbnails" = false;
+  "hide_author_avatars" = false;
+  "hide_clip_button" = "hidden";
+  "hide_comments_count" = false;
+  "hide_details" = false;
+  "hide_dislike_button" = "icons_only";
+  "hide_download_button" = "hidden";
+  "hide_footer" = false;
+  "hide_gradient_bottom" = false;
+  "hide_more_button" = true;
+  "hide_report_button" = true;
+  "hide_save_button" = "icons_only";
+  "hide_scroll_for_details" = false;
+  "hide_share_button" = "hidden";
+  "hide_shorts_remixing" = true;
+  "hide_sidebar" = false;
+  "hide_thanks_button" = "hidden";
+  "hide_voice_search_button" = true;
+  "how_long_ago_the_video_was_uploaded" = false;
+  "improvedtube_home" = "list";
+  "improvedtube_youtube_icon" = "disabled";
+  "join" = "hidden";
+  "lastDarkTheme" = "dark";
+  "lastLightTheme" = "default";
+  "likes" = "icons_only";
+  "livechat" = "hidden";
+  "mark_watched_videos" = false;
+  "no_page_margin" = false;
+  "only_one_player_instance_playing" = true;
+  "player_auto_cinema_mode" = false;
+  "player_auto_hide_cinema_mode_when_paused" = false;
+  "player_autofullscreen" = true;
+  "player_autopause_when_switching_tabs" = false;
+  "player_autoplay_button" = true;
+  "player_autoplay_disable" = false;
+  "player_cinema_mode_button" = false;
+  "player_fit_to_win_button" = false;
+  "player_forced_playback_speed" = true;
+  "player_forced_volume" = false;
+  "player_hamburger_button" = false;
+  "player_hide_cards" = false;
+  "player_loudness_normalization" = false;
+  "player_miniplayer_button" = true;
+  "player_play_button" = false;
+  "player_playback_speed" = 1.4;
+  "player_quality" = "disabled";
+  "player_quality_auto" = "migrated";
+  "player_remaining_duration" = true;
+  "player_remote_button" = false;
+  "player_rotate_button" = false;
+  "player_screen_button" = false;
+  "player_screenshot_button" = false;
+  "player_settings_button" = false;
+  "player_show_cards_on_mouse_hover" = true;
+  "player_size" = "do_not_change";
+  "player_transparent_background" = false;
+  "player_view_button" = true;
+  "playlist_reverse" = true;
+  "playlist_up_next_autoplay" = true;
+  "popup_ad" = false;
+  "purchase" = "hidden";
+  "red_dislike_button" = false;
+  "relatedVideosPrev" = "notTitles";
+  "related_videos" = "normal";
+  "remove_black_bars" = false;
+  "remove_history_shorts" = true;
+  "remove_home_page_shorts" = true;
+  "remove_shorts_reel_search_results" = true;
+  "remove_subscriptions_shorts" = true;
+  "remove_trending_shorts" = true;
+  "shortcut_reset_playback_speed" = {
+    "alt" = false;
+    "ctrl" = false;
+    "shift" = false;
+    "keys" = {
+      "61" = {
+        "code" = "NumpadEqual";
+        "key" = "=";
+      };
+    };
+    "wheel" = 0;
+  };
+  "shortcut_seek_backward" = {
+    "alt" = false;
+    "ctrl" = false;
+    "shift" = false;
+    "keys" = {
+      "188" = {
+        "code" = "Comma";
+        "key" = ",";
+      };
+    };
+    "wheel" = 0;
+  };
+  "shortcut_seek_forward" = {
+    "alt" = false;
+    "ctrl" = false;
+    "shift" = false;
+    "keys" = {
+      "190" = {
+        "code" = "Period";
+        "key" = ".";
+      };
+    };
+    "wheel" = 0;
+  };
+  "shortcuts_playback_speed_step" = 0.25;
+  "sidebar_left" = false;
+  "theme" = "dark";
+  "theme_primary_color" = [
+    200
+    200
+    200
+  ];
+  "theme_text_color" = [
+    25
+    25
+    25
+  ];
+  "thumbnails_hide" = false;
+  "thumbnails_right" = false;
+  "track_watched_videos" = false;
+  "transcript" = false;
+  "undo_the_new_sidebar" = true;
+  "up_next_autoplay" = false;
+  "youtubeDetailButtons" = "normal";
+  "youtube_home_page" = "/feed/subscriptions";
+}
