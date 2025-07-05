140055b27ce4ea21e476cbef5019fda2373e4036
repparw <ubritos@gmail.com>
2025-07-05 13:48:41 -0300

    rclone: crypt to gcrypt,ncrypt

--- modules/nixos/timers.nix
+++ modules/nixos/timers.nix
@@ -6,6 +6,9 @@
 }:
 let
   cfg = config.modules.timers;
+  rcloneCommand = destination: ''
+    ${lib.getExe pkgs.rclone} -L sync --exclude "qbittorrent/ipc-socket" --exclude "authelia/valkey/" --exclude "authelia/config/notification.txt" --exclude "authelia/config/users_database.yml" --exclude "traefik/certs/" --exclude "**/fail2ban/fail2ban.sqlite3" --exclude "**/letsencrypt/live/" --exclude "**/letsencrypt/archive/" --exclude "**/letsencrypt/accounts/" /home/repparw/.config/dlsuite ${destination}
+  '';
 in
 {
   options.modules.timers = {
@@ -22,13 +25,19 @@ in
             ExecStart = "${lib.getExe pkgs.rsync} -aq --delete /home/repparw/Pictures /home/repparw/Documents /home/repparw/.config --exclude='dlsuite' /mnt/hdd/backup";
           };
         };
-        rclone-sync = {
+        rclone-sync-gcrypt = {
           serviceConfig = {
             Type = "oneshot";
             User = "repparw";
-            ExecStart = ''
-              ${lib.getExe pkgs.rclone} -L sync --exclude "qbittorrent/ipc-socket" --exclude "authelia/valkey/" --exclude "authelia/config/notification.txt" --exclude "authelia/config/users_database.yml" --exclude "traefik/certs/" --exclude "**/fail2ban/fail2ban.sqlite3" --exclude "**/letsencrypt/live/" --exclude "**/letsencrypt/archive/" --exclude "**/letsencrypt/accounts/" /home/repparw/.config/dlsuite crypt:dlsuite
-            '';
+            ExecStart = rcloneCommand "gcrypt:dlsuite";
+          };
+        };
+
+        rclone-sync-ncrypt = {
+          serviceConfig = {
+            Type = "oneshot";
+            User = "repparw";
+            ExecStart = rcloneCommand "ncrypt:dlsuite";
           };
         };
 
@@ -57,10 +66,18 @@ in
           };
         };
 
-        rclone-sync = {
+        rclone-sync-gcrypt = {
           wantedBy = [ "timers.target" ];
           timerConfig = {
             OnCalendar = "*-*-7,14,21,28 04:00:00";
+            Persistent = true;
+          };
+        };
+
+        rclone-sync-ncrypt = {
+          wantedBy = [ "timers.target" ];
+          timerConfig = {
+            OnCalendar = "*-*-7,14,21,28 04:15:00";
             Persistent = true;
           };
         };
