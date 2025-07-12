08a732dfaf298e832fccec7a8ad1295621db757a
repparw <ubritos@gmail.com>
2025-07-12 15:09:05 -0300

    fix change time format

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -56,6 +56,14 @@
     ];
   };
 
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocales = [ "en_GB.UTF-8/UTF-8" ];
+    extraLocaleSettings = {
+      LC_TIME = "en_GB.UTF-8";
+    };
+  };
+
   nix = {
     # extraOptions = ''
     #   !include ${config.age.secrets.accessTokens.path}
