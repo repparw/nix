a420fdb832f20e8c7603cb6d36f9565963977fec
repparw <ubritos@gmail.com>
2025-07-18 10:18:41 -0300

    alpha: ryzen-smu

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -58,11 +58,11 @@
   };
 
   i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocales = [ "en_GB.UTF-8/UTF-8" ];
-    extraLocaleSettings = {
-      LC_TIME = "en_GB.UTF-8";
-    };
+    defaultLocale = "en_DK.UTF-8"; # en_SE not available in glibc?
+    # extraLocales = [ "en_GB.UTF-8/UTF-8" ];
+    # extraLocaleSettings = {
+    #   LC_TIME = "en_GB.UTF-8";
+    # };
   };
 
   nix = {
