ff2040194d6e69911c3e66294fc49398f68e94e5
repparw <ubritos@gmail.com>
2025-10-19 17:43:11 -0300

    fix(yazi): change sort binds to z, <C-p> to fzf

--- modules/hm/cli/file-manager.nix
+++ modules/hm/cli/file-manager.nix
@@ -31,13 +31,117 @@
           }
           {
             on = [ "<C-p>" ];
-            run = "plugin zoxide";
-            desc = "Jump to a directory via zoxide";
+            run = "plugin fzf";
+            desc = "Navigate with fzf";
           }
           {
             on = [ "!" ];
             run = "shell '$SHELL' --block";
             desc = "Open shell";
+          }
+          {
+            on = [
+              "z"
+              "m"
+            ];
+            run = "sort mtime --reverse=no linemode mtime";
+            desc = "Sort by modified time";
+          }
+          {
+            on = [
+              "z"
+              "M"
+            ];
+            run = "sort mtime --reverse linemode mtime";
+            desc = "Sort by modified time (reverse)";
+          }
+          {
+            on = [
+              "z"
+              "b"
+            ];
+            run = "sort btime --reverse=no linemode btime";
+            desc = "Sort by birth time";
+          }
+          {
+            on = [
+              "z"
+              "B"
+            ];
+            run = "sort btime --reverse linemode btime";
+            desc = "Sort by birth time (reverse)";
+          }
+          {
+            on = [
+              "z"
+              "e"
+            ];
+            run = "sort extension --reverse=no";
+            desc = "Sort by extension";
+          }
+          {
+            on = [
+              "z"
+              "E"
+            ];
+            run = "sort extension --reverse";
+            desc = "Sort by extension (reverse)";
+          }
+          {
+            on = [
+              "z"
+              "a"
+            ];
+            run = "sort alphabetical --reverse=no";
+            desc = "Sort alphabetically";
+          }
+          {
+            on = [
+              "z"
+              "A"
+            ];
+            run = "sort alphabetical --reverse";
+            desc = "Sort alphabetically (reverse)";
+          }
+          {
+            on = [
+              "z"
+              "n"
+            ];
+            run = "sort natural --reverse=no";
+            desc = "Sort naturally";
+          }
+          {
+            on = [
+              "z"
+              "N"
+            ];
+            run = "sort natural --reverse";
+            desc = "Sort naturally (reverse)";
+          }
+          {
+            on = [
+              "z"
+              "s"
+            ];
+            run = "sort size --reverse=no linemode size";
+            desc = "Sort by size";
+          }
+          {
+            on = [
+              "z"
+              "S"
+            ];
+            run = "sort size --reverse linemode size";
+            desc = "Sort by size (reverse)";
+          }
+          {
+            on = [
+              "z"
+              "r"
+            ];
+            run = "sort random --reverse=no";
+            desc = "Sort randomly";
           }
         ];
       };
