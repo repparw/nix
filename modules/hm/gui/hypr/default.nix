da6f31bc76284d2379f860005d7dfa368c9e92b4
repparw <ubritos@gmail.com>
2025-10-28 10:23:54 -0300

    chore(kitty): rm hard coded term

--- modules/hm/gui/hypr/default.nix
+++ modules/hm/gui/hypr/default.nix
@@ -67,7 +67,7 @@
         "$screenoff" = "sleep 3 && hyprctl dispatch dpms off";
 
         # Terminal
-        "$terminal" = "$prefix kitty";
+        "$terminal" = "$prefix $term";
         "$shell" = "fish";
         "$top" = "$terminal $shell -ic top";
         "$fileManager" = "hdrop $terminal --class filemanager $shell -ic yazi";
