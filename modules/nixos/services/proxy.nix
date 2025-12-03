fec54878dd79b149e3e8d5af76191ecf94d18440
repparw <ubritos@gmail.com>
2025-12-03 15:51:21 -0300

    services(traefik): disable api dashboard

--- modules/nixos/services/proxy.nix
+++ modules/nixos/services/proxy.nix
@@ -26,10 +26,6 @@
       "traefik.tls.stores.default.defaultgeneratedcert.resolver" = "cloudflare";
       "traefik.tls.stores.default.defaultgeneratedcert.domain.main" = "${cfg.domain}";
       "traefik.tls.stores.default.defaultgeneratedcert.domain.sans" = "*.${cfg.domain}";
-      # api
-      "traefik.http.routers.api.rule" = "Host(`traefik.${cfg.domain}`)";
-      "traefik.http.routers.api.tls" = "true";
-      "traefik.http.routers.api.service" = "api@internal";
       # auth middleware
       # defined here as authelia can start after traefik
       "traefik.http.middlewares.authelia.forwardAuth.address" =
