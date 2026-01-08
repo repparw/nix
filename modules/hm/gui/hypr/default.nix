836bec59190ae92c899c1783b361f2fbe163633b
repparw <ubritos@gmail.com>
2026-01-08 19:37:49 -0300

    hypr: disable hyprsplit

--- modules/hm/gui/hypr/default.nix
+++ modules/hm/gui/hypr/default.nix
@@ -19,7 +19,7 @@
       plugins = with pkgs.hyprlandPlugins; [
         # change requires hyprland restart
         hyprexpo
-        hyprsplit
+        # hyprsplit
       ];
 
       systemd.variables = [ "--all" ];
