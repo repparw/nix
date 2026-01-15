6ae491314ee0f2df956ec6c2804a4c0ab3133837
repparw <ubritos@gmail.com>
2026-01-15 00:36:09 -0300

    Revert "chore(spotify): +librespot, -spotifyd"
    This reverts commit da2f6d5634255ee5c33c366842faa5ce16a50d26.

--- home/alpha.nix
+++ home/alpha.nix
@@ -1,4 +1,12 @@
 _: {
+<<<<<<< HEAD
   services.librespot.enable = true;
   services.jellyfin-mpv-shim.enable = false;
+||||||| da2f6d563 (chore(spotify): +librespot, -spotifyd)
+  services.librespot.enable = true;
+  services.jellyfin-mpv-shim.enable = true;
+=======
+  services.spotifyd.enable = true;
+  services.jellyfin-mpv-shim.enable = true;
+>>>>>>> parent of da2f6d563 (chore(spotify): +librespot, -spotifyd)
 }
