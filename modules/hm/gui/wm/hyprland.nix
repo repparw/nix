5d5b3f7005ad24cbbb445b075ec111eadfdfabbb
repparw <ubritos@gmail.com>
2026-02-15 12:13:48 -0300

    hyprland: launch cfait directly in terminal

--- modules/hm/gui/wm/hyprland.nix
+++ modules/hm/gui/wm/hyprland.nix
@@ -185,7 +185,7 @@
 
           "$mod, A, exec, $prefix anki"
           "$mod, C, exec, $whatsapp"
-          "$mod, X, exec, $prefix cfait"
+          "$mod, X, exec, $terminal cfait"
           "$mod, G, exec, webapp https://gemini.google.com/app"
           "$mod, RETURN, exec, $terminal"
           "$mod, W, killactive,"
