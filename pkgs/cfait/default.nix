4fc8309cbc29fb2b339967dcb3d1bef97b137db7
repparw <ubritos@gmail.com>
2026-02-14 23:19:23 -0300

    pkgs: add cfait package

---
+++ pkgs/cfait/default.nix
@@ -0,0 +1,66 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  openssl,
+  sqlite,
+  libxkbcommon,
+  wayland,
+  libx11,
+  libxcursor,
+  libxrandr,
+  libxi,
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "cfait";
+  version = "0.4.9";
+
+  src = fetchFromGitHub {
+    owner = "trougnouf";
+    repo = "cfait";
+    rev = "v${version}";
+    hash = "sha256-7cbJ5HEkGhjIOnIeBEasa5TVurXcSLuOB3rOMLFd+os=";
+  };
+
+  cargoHash = "sha256-2P9ybgIIvG7oaVOkvPaWNYyIMfa9PFlJbnLaGe4IYjc=";
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    openssl
+    sqlite
+    libxkbcommon
+    wayland
+    libx11
+    libxcursor
+    libxrandr
+    libxi
+  ];
+
+  buildFeatures = [ "gui" ];
+
+  cargoBuildFlags = [
+    "--bin"
+    "cfait"
+    "--bin"
+    "gui"
+  ];
+
+  postInstall = ''
+    mv $out/bin/gui $out/bin/cfait-gui
+  '';
+
+  doCheck = false;
+
+  meta = {
+    description = "A powerful, fast and elegant task manager (CalDAV and local, GUI, TUI, and Android clients)";
+    homepage = "https://codeberg.org/trougnouf/cfait";
+    license = lib.licenses.gpl3Only;
+    maintainers = [ ];
+    mainProgram = "cfait";
+  };
+}
