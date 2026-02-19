ea5812894dbbaa7793813aa24bf70a327f0da140
repparw <ubritos@gmail.com>
2026-02-19 16:49:00 -0300

    gui: disable hyprland, add xwayland-satellite, enable steam args

--- modules/nixos/gui/niri.nix
+++ modules/nixos/gui/niri.nix
@@ -1,10 +1,13 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 {
   config = lib.mkIf config.modules.gui.enable {
     programs.niri.enable = lib.mkDefault true;
+
+    environment.systemPackages = [ pkgs.xwayland-satellite ];
   };
 }
