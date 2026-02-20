54941330b613b9dc7e7c944708bc0ed35f27c316
repparw <ubritos@gmail.com>
2026-02-20 12:54:58 -0300

    pkgs: add native-client for browser extensions

---
+++ pkgs/native-client/default.nix
@@ -0,0 +1,101 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  nodejs,
+  jq,
+  makeWrapper,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "native-client";
+  version = "1.0.7";
+
+  src = fetchFromGitHub {
+    owner = "andy-portmen";
+    repo = "native-client";
+    rev = "v${version}";
+    hash = "sha256-azCdJS6gwHeqBQgIsAhjX/J5NpoqeFt7nwtBoqB/56Y=";
+  };
+
+  nativeBuildInputs = [
+    jq
+    nodejs
+    makeWrapper
+  ];
+
+  phases = [ "buildPhase" ];
+
+  buildPhase = ''
+    # code
+    hostDir=$out/libexec/native-client
+    firefoxHosts=$out/lib/mozilla/native-messaging-hosts
+    chromeHosts=$out/lib/chromium/NativeMessagingHosts
+
+    # For home-manager compatibility
+    hmChromeHosts=$out/etc/chromium/native-messaging-hosts
+
+    mkdir -p "$hostDir" "$firefoxHosts" "$chromeHosts" "$hmChromeHosts"
+    # Keep manifests clean; only manifests live in native-messaging-hosts.
+    install -Dm444 -t "$hostDir" \
+      $src/config.js \
+      $src/host.js \
+      $src/messaging.js
+
+    APP_NAME="com.add0n.node"
+    APP_DESCRIPTION="Node Host for Native Messaging"
+    BINARY=$out/bin/run.sh
+
+    # Mediator manifests
+    get_extension_ids() {
+      node -e '
+        const file = process.argv[1]
+        const key = process.argv[2]
+        const config = require(file)
+        console.log(JSON.stringify(config.ids[key], null, 2))
+      ' "$hostDir/config.js" "$1"
+    }
+
+    EXT_FIREFOX=$(get_extension_ids firefox)
+    jq -ner \
+      --arg name "$APP_NAME" \
+      --arg description "$APP_DESCRIPTION" \
+      --arg bin "$BINARY" \
+      --argjson ext "$EXT_FIREFOX" '
+      {
+        "name": $name,
+        "description": $description,
+        "path": $bin,
+        "type": "stdio",
+        "allowed_extensions": $ext
+      }' > "$firefoxHosts/$APP_NAME.json"
+
+    EXT_CHROME=$(get_extension_ids chrome)
+    jq -ner \
+      --arg name "$APP_NAME" \
+      --arg description "$APP_DESCRIPTION" \
+      --arg bin "$BINARY" \
+      --argjson ext "$EXT_CHROME" '
+      {
+        "name": $name,
+        "description": $description,
+        "path": $bin,
+        "type": "stdio",
+        "allowed_origins": ($ext | map("chrome-extension://" + . + "/"))
+      }' > "$chromeHosts/$APP_NAME.json"
+
+    cp "$chromeHosts/$APP_NAME.json" "$hmChromeHosts/"
+
+    makeWrapper ${nodejs}/bin/node "$BINARY" \
+      --add-flags "$hostDir/host.js"
+  '';
+
+  meta = {
+    description = "Native Messaging component for Windows, Linux, and Mac OS that is written in NodeJS";
+    homepage = "https://github.com/andy-portmen/native-client";
+    license = lib.licenses.mpl20;
+    maintainers = with lib.maintainers; [ pschmitt ];
+    # mainProgram = "run.sh";
+    platforms = lib.platforms.all;
+  };
+}
