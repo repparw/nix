1dba33dcb0e9163ef239d988cadd8cffb79eaa79
repparw <ubritos@gmail.com>
2026-02-25 17:10:17 -0300

    chore(style): refactor, theme to tokyodark-terminal

--- modules/nixos/style.nix
+++ modules/nixos/style.nix
@@ -1,83 +1,94 @@
 {
   pkgs,
+  lib,
+  config,
   ...
 }:
 {
-  stylix = {
-    enable = true;
+  stylix =
+    let
+      # INFO Colorscheme change
+      theme = "${pkgs.base16-schemes}/share/themes/tokyodark-terminal.yaml";
 
-    # INFO Colorscheme change
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyodark.yaml";
+      # wallpaper = config.lib.stylix.pixel "base00"
+      # wallpaper = ./rocket-surgery.png;
 
-    # image = config.lib.stylix.pixel "base00";
+      baseUrl = "https://codeberg.org/exorcist/wallpapers/raw/commit/7b9a7511705ecd7f055fb007c467a2a66f612ca3";
 
-    image = ./rocket-surgery.png;
-    # let
-    #   baseUrl = "https://codeberg.org/exorcist/wallpapers/raw/commit/7b9a7511705ecd7f055fb007c467a2a66f612ca3";
-    #
-    #   wallpaperOptions = {
-    #     dalek = "sha256-Rd30EHETeTS2+h2/8ii/+Gc29dpFHQgeYajIHqO3C9c=";
-    #     penguin = "sha256-rTE57xA9FD6AuUCRH3HKJhXDNwm5fu4WMBeW9ocUM+A=";
-    #     dead-robot = "sha256-WYWVgp6w4mQIzJOZXncacCSl4tm3sum3vJxvZ8gn+9I=";
-    #     forest-4 = "sha256-mqrwRvJmRLK3iyEiXmaw5UQPftEaqg33NhwzpZvyXws=";
-    #     houses = "sha256-p5Mo1xA4jBZh6PPP0HK2YsuEBkP/gA27YDvxtuUrPHE=";
-    #     solar-system-2 = "sha256-8aVsWogIUuu6rEvGtEJ1y0NojJhEkbeAU87yPFn0d1g=";
-    #     terminal-redux = "sha256-1AbBA2Lufl2gxxfn6zzkQ3/yS6gXer0rOvYMP9EdHnE=";
-    #   };
-    #
-    #   fetchSelectedWallpaper =
-    #     name:
-    #     pkgs.fetchurl {
-    #       url = "${baseUrl}/gruvbox/${name}.jpg";
-    #       hash = wallpaperOptions.${name};
-    #     };
-    #
-    #   selectedWallpaper = fetchSelectedWallpaper "terminal-redux";
-    #
-    #   # Brightness and contrast settings
-    #   brightness = -10;
-    #   contrast = 0;
-    #   fillColor = "black";
-    #
-    #   # Process the image with brightness/contrast adjustments
-    #   wallpaper = pkgs.runCommand "dimmed-wallpaper.jpg" { } ''
-    #     ${lib.getExe' pkgs.imagemagick "convert"} "${selectedWallpaper}" -brightness-contrast ${toString brightness},${toString contrast} -fill ${fillColor} $out
-    #   '';
-    # in
-    # wallpaper;
-    fonts = {
-      sansSerif = {
-        name = "FiraCode Nerd Font";
-        package = pkgs.nerd-fonts.fira-code;
-      };
-      serif = {
-        name = "FiraCode Nerd Font";
-        package = pkgs.nerd-fonts.fira-code;
-      };
-      monospace = {
-        name = "FiraCode Nerd Font Mono";
-        package = pkgs.nerd-fonts.fira-code;
+      wallpaperOptions = {
+        gruvbox = {
+          dalek = "sha256-Rd30EHETeTS2+h2/8ii/+Gc29dpFHQgeYajIHqO3C9c=";
+          penguin = "sha256-rTE57xA9FD6AuUCRH3HKJhXDNwm5fu4WMBeW9ocUM+A=";
+          dead-robot = "sha256-WYWVgp6w4mQIzJOZXncacCSl4tm3sum3vJxvZ8gn+9I=";
+          forest-4 = "sha256-mqrwRvJmRLK3iyEiXmaw5UQPftEaqg33NhwzpZvyXws=";
+          houses = "sha256-p5Mo1xA4jBZh6PPP0HK2YsuEBkP/gA27YDvxtuUrPHE=";
+          solar-system-2 = "sha256-8aVsWogIUuu6rEvGtEJ1y0NojJhEkbeAU87yPFn0d1g=";
+          terminal-redux = "sha256-1AbBA2Lufl2gxxfn6zzkQ3/yS6gXer0rOvYMP9EdHnE=";
+        };
+        stalenhag = {
+          "10 - tt8vpLm" = "sha256-KO11mCmICedw8icmfM1J3BehX13OGQbQNkd/8S+J6WU=";
+        };
       };
 
-      sizes = {
-        applications = 12;
-        desktop = 12;
-        terminal = 12;
-        popups = 12;
+      fetchSelectedWallpaper =
+        group: name:
+        pkgs.fetchurl {
+          url = "${baseUrl}/${group}/${lib.escapeURL name}.jpg";
+          hash = wallpaperOptions.${group}.${name};
+        };
+
+      inputImage = fetchSelectedWallpaper "stalenhag" "10 - tt8vpLm";
+      brightness = -15;
+      contrast = 0;
+      fillColor = "black";
+
+      wallpaper = pkgs.runCommand "dimmed-wallpaper.jpg" { } ''
+        ${lib.getExe' pkgs.imagemagick "convert"} "${inputImage}" -brightness-contrast ${toString brightness},${toString contrast} -fill ${fillColor} $out
+      '';
+    in
+    {
+      enable = true;
+
+      base16Scheme = theme;
+      image = wallpaper;
+
+      fonts = {
+        sansSerif = {
+          name = "FiraCode Nerd Font";
+          package = pkgs.nerd-fonts.fira-code;
+        };
+        serif = {
+          name = "FiraCode Nerd Font";
+          package = pkgs.nerd-fonts.fira-code;
+        };
+        monospace = {
+          name = "FiraCode Nerd Font Mono";
+          package = pkgs.nerd-fonts.fira-code;
+        };
+
+        sizes = {
+          applications = 12;
+          desktop = 12;
+          terminal = 12;
+          popups = 12;
+        };
+      };
+
+      cursor = {
+        # package = pkgs.simp1e-cursors;
+        # name = "Simp1e-Tokyo-Night";
+        # name = "Simp1e-Rose-Pine";
+        # name = "Simp1e-Catppuccin-Mocha";
+        package = pkgs.rose-pine-cursor;
+        name = "BreezeX-RosePine-Linux";
+        size = 24;
+      };
+
+      opacity = {
+        terminal = 0.95;
+        popups = 0.75;
       };
     };
-
-    cursor = {
-      name = "Capitaine Cursors (Gruvbox)";
-      package = pkgs.capitaine-cursors-themed;
-      size = 24;
-    };
-
-    opacity = {
-      terminal = 0.9;
-      popups = 0.75;
-    };
-  };
   home-manager.sharedModules = [
     {
       home.pointerCursor.dotIcons.enable = false;
@@ -94,10 +105,13 @@
             transparentBackground.numberLine = true;
             transparentBackground.signColumn = true;
           };
-          firefox.profileNames = [
-            "default"
-            "kiosk"
-          ];
+          firefox = {
+            colorTheme.enable = true;
+            profileNames = [
+              "default"
+              "kiosk"
+            ];
+          };
           hyprlock.useWallpaper = false;
           waybar = {
             enableCenterBackColors = true;
