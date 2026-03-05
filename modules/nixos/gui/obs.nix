feadb99952f3408210dcaed74b33c2872d32b5f3
repparw <ubritos@gmail.com>
2026-03-05 20:20:36 -0300

    feat(obs): +bacgkroundremoval

--- modules/nixos/gui/obs.nix
+++ modules/nixos/gui/obs.nix
@@ -8,10 +8,9 @@
   config = lib.mkIf config.modules.gui.enable {
     programs.obs-studio = {
       enableVirtualCamera = true;
-      # plugins = with pkgs.obs-studio-plugins; [
-      #   obs-pipewire-audio-capture
-      #   obs-backgroundremoval
-      # ];
+      plugins = with pkgs.obs-studio-plugins; [
+        obs-backgroundremoval
+      ];
     };
   };
 }
