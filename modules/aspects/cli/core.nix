4dfaf09733fabc64f3be3c8dde39b97a094b97ac
repparw <ubritos@gmail.com>
2026-03-21 15:34:12 -0300

    fixup! move ssh config to dedicated aspect

--- modules/aspects/cli/core.nix
+++ modules/aspects/cli/core.nix
@@ -84,21 +84,5 @@
           };
         };
       };
-
-    homeManager =
-      { ... }:
-      {
-        programs.ssh = {
-          enable = true;
-          enableDefaultConfig = false;
-
-          matchBlocks = {
-            pi = {
-              hostname = "192.168.0.4";
-              user = "repparw";
-            };
-          };
-        };
-      };
   };
 }
