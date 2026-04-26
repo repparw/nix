eb2c4d21fa2e20c0b04da77533e60ace0b75495c
repparw <ubritos@gmail.com>
2026-04-26 16:22:01 -0300

    feat(services): use host-aspects for alpha homeManager projection
    - Add den._.host-aspects to repparw includes
    - Remove explicit provides.alpha.includes routing for nixos-services
    - Move spotifyd config from provides.repparw.homeManager to alpha.homeManager
      so host-aspects projects it to repparw automatically

--- modules/hosts/alpha.nix
+++ modules/hosts/alpha.nix
@@ -16,6 +16,25 @@
       den.aspects.virtual-display
     ];
 
+    homeManager = {
+      services.spotifyd = {
+        enable = true;
+        settings.global = {
+          username = "REDACTED";
+          device_name = "alpha";
+          bitrate = 320;
+          max_cache_size = 5000000000;
+          initial_volume = 70;
+          volume_normalisation = false;
+        };
+      };
+
+      systemd.user.services.spotifyd = {
+        Unit.After = [ "network-online.target" ];
+        Service.RuntimeMaxSec = "6h";
+      };
+    };
+
     nixos =
       {
         config,
