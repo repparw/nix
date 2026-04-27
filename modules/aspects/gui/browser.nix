2ff6d2af193126ce4b4ad729d6a918bc66f5040a
repparw <ubritos@gmail.com>
2026-04-27 15:39:44 -0300

    feat(browser): migrate kiosk from firefox to chromium pwa

--- modules/aspects/gui/browser.nix
+++ modules/aspects/gui/browser.nix
@@ -223,50 +223,6 @@
                     default = "google";
                   };
                 };
-                kiosk = commonProfile // {
-                  id = 1;
-                  path = "kiosk";
-                  extensions = with pkgs.firefox-addons; {
-                    force = true;
-                    settings = {
-                      "sponsorblock@ajay.app".settings = {
-                        hideUploadButtonPlayerControls = true;
-                        dontShowNotice = true;
-                      };
-                      "{3c6bf0cc-3ae2-42fb-9993-0d33104fdcaf}".settings = {
-                        "below_player_loop" = false;
-                        "below_player_pip" = false;
-                        "below_player_screenshot" = false;
-                        "channel_default_tab" = "/videos";
-                        "hide_clip_button" = "hidden";
-                        "hide_report_button" = true;
-                        "hide_shorts_remixing" = true;
-                        "hide_voice_search_button" = true;
-                        "join" = "hidden";
-                        "only_one_player_instance_playing" = true;
-                        "player_autofullscreen" = true;
-                        "player_forced_playback_speed" = true;
-                        "player_playback_speed" = 1.4;
-                        "remove_history_shorts" = true;
-                        "remove_home_page_shorts" = true;
-                        "remove_member_only" = true;
-                        "remove_shorts_reel_search_results" = true;
-                        "remove_subscriptions_shorts" = true;
-                        "remove_trending_shorts" = true;
-                        "theme" = "dark";
-                        "track_watched_videos" = false;
-                        "transcript" = false;
-                        "youtube_home_page" = "/feed/subscriptions";
-                      };
-                    };
-                    packages = [
-                      youtube-addon
-                      sponsorblock
-                      dearrow
-                      tampermonkey
-                    ];
-                  };
-                };
               };
           };
 
