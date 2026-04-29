f24d79b08f5e2ae71227b59b358857e128ce2617
repparw <ubritos@gmail.com>
2026-04-29 01:09:00 -0300

    feat(streaming): use lg cx hdr edid and dynamic gamescope resolution
    - Switch virtual display from edid-generator to LG CX EDID with HDR10
      and BT2020 metadata so the kernel reports an HDR-capable connector
    - Change default virtual display resolution to 3840x2160@120
    - Drive gamescope height and refresh from modules.virtualDisplay config
      instead of hardcoded values
    - Remove hardcoded 1440p mode override from niri-output-on script

--- modules/aspects/streaming/niri-output-on.sh
+++ modules/aspects/streaming/niri-output-on.sh
@@ -1,3 +1,2 @@
 export $(systemctl --user show-environment | grep '^NIRI_SOCKET=')
 niri msg output DP-2 on
-niri msg output DP-2 mode "2560x1440@119.986"
--- modules/aspects/virtual-display.nix
+++ modules/aspects/virtual-display.nix
@@ -6,6 +6,13 @@
   den.aspects.virtual-display = {
     nixos =
       { config, pkgs, ... }:
+      let
+        lg-oled-cx-edid = pkgs.fetchurl {
+          name = "lg-oled-cx-hdmi.bin";
+          url = "https://git.linuxtv.org/v4l-utils.git/plain/utils/edid-decode/data/lg-oled-cx-hdmi";
+          sha256 = "0ymv630xgd5xnwpkkyz4mx3hwxnbw06rad9v1s4xvrzrqnin3b4i";
+        };
+      in
       {
         options.modules.virtualDisplay = {
           port = lib.mkOption {
@@ -15,7 +22,7 @@
           };
           resolution = lib.mkOption {
             type = lib.types.str;
-            default = "2560x1440";
+            default = "3840x2160";
             description = "Resolution for virtual display";
           };
           refreshRate = lib.mkOption {
@@ -26,7 +33,12 @@
         };
 
         config = {
-          hardware.firmware = [ pkgs.edid-generator ];
+          hardware.firmware = [
+            (pkgs.runCommand "lg-oled-cx-edid" { } ''
+              mkdir -p $out/lib/firmware/edid
+              cp ${lg-oled-cx-edid} $out/lib/firmware/edid/${config.modules.virtualDisplay.resolution}.bin
+            '')
+          ];
           boot.kernelParams = [
             "drm.edid_firmware=${config.modules.virtualDisplay.port}:edid/${config.modules.virtualDisplay.resolution}.bin"
             "video=${config.modules.virtualDisplay.port}:${config.modules.virtualDisplay.resolution}@${toString config.modules.virtualDisplay.refreshRate}e"
