832b041151f9a5684e91c8d623ff932745e50872
repparw <ubritos@gmail.com>
2026-05-01 16:10:59 -0300

    feat(tasks): filter todo list to only show tasks with due dates

--- modules/aspects/cli/tasks.nix
+++ modules/aspects/cli/tasks.nix
@@ -84,6 +84,16 @@
             default_due = 24
           '';
         };
+
+        programs.fish.functions.todo = ''
+          if test (count $argv) -eq 0
+            command todo list --due 999999
+          else if test "$argv[1]" = "list"
+            command todo list --due 999999 $argv[2..]
+          else
+            command todo $argv
+          end
+        '';
       };
   };
 }
