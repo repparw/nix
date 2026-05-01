e5504a36fa10b855b798562cacee1a4bf86e1c88
repparw <ubritos@gmail.com>
2026-05-01 16:10:59 -0300

    feat(tasks): filter todo list to only show tasks with due dates

--- modules/aspects/nix.nix
+++ modules/aspects/nix.nix
@@ -6,10 +6,12 @@ _: {
           extra-substituters = [
             "https://cachix.cachix.org"
             "https://devenv.cachix.org"
+            "https://nix-community.cachix.org"
           ];
           extra-trusted-public-keys = [
             "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
             "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           ];
           use-xdg-base-directories = true;
           trusted-users = [ "root" ];
