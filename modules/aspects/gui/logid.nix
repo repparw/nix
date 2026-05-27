63d0c904f69dfccd32ffc2507a4da2f0f37aa445
repparw <ubritos@gmail.com>
2026-05-27 13:17:11 -0300

    chore(gui): migrate to upstream logiops service

--- modules/aspects/gui/logid.nix
+++ modules/aspects/gui/logid.nix
@@ -1,64 +1,45 @@
 {
   den,
-  lib,
-  pkgs,
   ...
 }:
 {
   den.aspects.logid = {
     includes = [ ];
 
-    nixos =
-      { pkgs, ... }:
-      {
-        environment.etc = {
-          "logid.cfg" = {
-            text = ''
-              devices: (
-              {
-                name: "MX Vertical Advanced Ergonomic Mouse";
-                smartshift:
+    nixos = {
+      services.logiops = {
+        enable = true;
+        config = {
+          devices = [
+            {
+              name = "MX Vertical Advanced Ergonomic Mouse";
+              smartshift = {
+                on = true;
+                threshold = 30;
+              };
+              hiresscroll = {
+                hires = true;
+                invert = false;
+                target = false;
+              };
+              dpi = 1600;
+              buttons = [
                 {
-                  on: true;
-                  threshold: 30;
-                };
-                hiresscroll:
-                {
-                  hires: true;
-                  invert: false;
-                  target: false;
-                };
-                dpi: 1600;
-
-                buttons: (
-                  {
-                    cid: 0xfd;
-                    action =
-                    {
-                      type = "Keypress";
-                      keys: ["KEY_LEFTSHIFT", "KEY_LEFTMETA", "KEY_PRINT"];
-                    };
-                  }
-                );
-              }
-              );
-            '';
-          };
-        };
-
-        systemd.services.logid = {
-          startLimitIntervalSec = 0;
-          after = [
-            "graphical.target"
-            "systemd-udev-settle.service"
+                  cid = 253;
+                  action = {
+                    type = "Keypress";
+                    keys = [
+                      "KEY_LEFTSHIFT"
+                      "KEY_LEFTMETA"
+                      "KEY_PRINT"
+                    ];
+                  };
+                }
+              ];
+            }
           ];
-          wantedBy = [ "graphical.target" ];
-          serviceConfig = {
-            Type = "simple";
-            ExecStart = "${lib.getExe pkgs.logiops_0_2_3}";
-            ExecStartPre = "${lib.getExe' pkgs.coreutils "sleep"} 2";
-          };
         };
       };
+    };
   };
 }
