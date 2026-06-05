9549f88d14791e9402905d8620d3233e88f16072
repparw <ubritos@gmail.com>
2026-06-05 19:10:15 -0300

    docs: expand repo runbook and checks

--- modules/checks.nix
+++ modules/checks.nix
@@ -1,20 +1,34 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 {
   perSystem =
     { config, pkgs, ... }:
     {
-      checks.formatting =
-        pkgs.runCommand "check-formatting"
-          {
-            nativeBuildInputs = [ config.formatter ];
-          }
-          ''
-            export HOME=$(mktemp -d)
-            cp -r ${inputs.self} src
-            chmod -R +w src
-            cd src
-            treefmt --tree-root . --walk filesystem --fail-on-change
-            touch $out
-          '';
+      checks =
+        let
+          hosts = lib.attrNames inputs.self.nixosConfigurations;
+          evalHost =
+            host:
+            pkgs.runCommand "check-nixos-${host}-eval" { } ''
+              printf '%s\n' '${
+                inputs.self.nixosConfigurations.${host}.config.system.build.toplevel.drvPath
+              }' > $out
+            '';
+        in
+        {
+          formatting =
+            pkgs.runCommand "check-formatting"
+              {
+                nativeBuildInputs = [ config.formatter ];
+              }
+              ''
+                export HOME=$(mktemp -d)
+                cp -r ${inputs.self} src
+                chmod -R +w src
+                cd src
+                treefmt --tree-root . --walk filesystem --fail-on-change
+                touch $out
+              '';
+        }
+        // lib.genAttrs hosts evalHost;
     };
 }
