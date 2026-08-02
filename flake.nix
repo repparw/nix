a6a60c376f80393ad8e7d5816352f3aba5923a7e
repparw <ubritos@gmail.com>
2026-08-02 20:52:19 -0300

    fix(streaming): drop obsolete Moonshine input

--- flake.nix
+++ flake.nix
@@ -41,10 +41,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     import-tree.url = "github:vic/import-tree";
-    moonshine = {
-      url = "github:hgaiser/moonshine";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
