57d9c9acd89db91427fcd0236d5a179087845145
repparw <ubritos@gmail.com>
2026-08-15 11:47:55 -0300

    chore(aspects/audio): drop avantree codec priority override

--- modules/aspects/audio.nix
+++ modules/aspects/audio.nix
@@ -23,26 +23,6 @@
                 };
               };
 
-              preferAvantreeCodec = {
-                "device.profile.priority.rules" = [
-                  {
-                    matches = [
-                      { "api.bluez5.address" = "REDACTED"; }
-                    ];
-                    actions.update-props = {
-                      # Avoid aptX-LL, whose transport fails on this device.
-                      priorities = [
-                        "a2dp-sink"
-                        "a2dp-sink-sbc_xq"
-                        "a2dp-sink-sbc"
-                        "a2dp-sink-aac"
-                        "headset-head-unit"
-                      ];
-                    };
-                  }
-                ];
-              };
-
               disableHdmi = {
                 "monitor.alsa.rules" = [
                   {
