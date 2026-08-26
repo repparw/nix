071007688330e32f2dbfc7df29e37137130751d3
repparw <ubritos@gmail.com>
2026-08-26 20:46:37 -0300

    chore(backup): consolidate offsite restic and rclone configuration

--- modules/aspects/gui/default.nix
+++ modules/aspects/gui/default.nix
@@ -1,16 +1,23 @@
 { den, ... }:
 {
   den.aspects.gui = {
-    includes = with den.aspects.gui._; [
-      session
-      niri
-      browser
-      mpv
-      spotify
-      wm
-      zathura
-      guiApps
-    ];
+    includes =
+      with den.aspects.gui._;
+      [
+        session
+        niri
+        browser
+        mpv
+        spotify
+        wm
+        zathura
+        guiApps
+      ]
+      # Sound and theming only exist where a GUI does; headless hosts skip both.
+      ++ (with den.aspects; [
+        audio
+        style
+      ]);
   };
 
   den.aspects.gui.provides.session = {
