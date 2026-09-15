ce261b64400241f9ad009a08ee45dd611400d3fb
repparw <ubritos@gmail.com>
2026-09-15 20:51:17 -0300

    feat(browser): replace Chromium with Helium

--- modules/aspects/gui/niri.nix
+++ modules/aspects/gui/niri.nix
@@ -233,14 +233,9 @@
               ];
               "Mod+X" = titledSpawn "Tasks" [ "tasks-org" ];
               "Mod+Y" = titledSpawn "YouTube" [
-                "ndrop"
-                "-F"
-                "-c"
-                "chrome-agimnkijcaahngcdmfeangaknmldooml-Default"
-                "chromium"
-                "--password-store=basic"
-                "--profile-directory=Default"
-                "--app-id=agimnkijcaahngcdmfeangaknmldooml"
+                "webapp"
+                "youtube"
+                "https://www.youtube.com"
               ];
               "Mod+Z" = titledSpawn "MPV Clipboard" [ "mpvclip" ];
 
@@ -435,7 +430,7 @@
               }
               {
                 window-rule = {
-                  match._props.app-id = "chrome-agimnkijcaahngcdmfeangaknmldooml-Default";
+                  match._props.app-id = "youtube";
                   open-on-output = "HDMI-A-1";
                   default-column-width.proportion = 1.0;
                 };
