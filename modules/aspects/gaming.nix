9a830f6a00ae3f018039ecb0dbdd6839330727e4
repparw <ubritos@gmail.com>
2026-09-17 10:03:08 -0300

    fix(gaming): pin GE-Proton11-7 for x86_64
    Override proton-ge-bin src to the GE-Proton11-7 release tarball; non-x86_64 keeps stock proton-ge-bin.

--- modules/aspects/gaming.nix
+++ modules/aspects/gaming.nix
@@ -7,13 +7,26 @@
   den.aspects.gaming = {
     nixos =
       { pkgs, ... }:
+      let
+        protonGe =
+          if pkgs.stdenv.hostPlatform.isx86_64 then
+            pkgs.proton-ge-bin.overrideAttrs {
+              version = "GE-Proton11-7";
+              src = pkgs.fetchzip {
+                url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton11-7/GE-Proton11-7-x86_64.tar.gz";
+                hash = "sha256-ftW0vE45v2JsbaYqo/So0ZFfvdtakHX0XEXEE4TdxLk=";
+              };
+            }
+          else
+            pkgs.proton-ge-bin;
+      in
       {
         boot.kernelModules = [ "ntsync" ];
         hardware.xpadneo.enable = true;
         programs = {
           steam = {
             enable = true;
-            extraCompatPackages = with pkgs; [ proton-ge-bin ];
+            extraCompatPackages = [ protonGe ];
             remotePlay.openFirewall = true;
             localNetworkGameTransfers.openFirewall = true;
           };
@@ -30,7 +43,7 @@
                 gamescope
                 gamemode
                 mangohud
-                proton-ge-bin
+                protonGe
               ];
           })
         ];
